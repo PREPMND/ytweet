9a03f7832e55d3761b445e51165ed0bd2645d30b
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:06:58 +0000

    Apr 8, 2026, 10:36 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -14,8 +14,8 @@ const Login = () => {
                                 withCredentials: true, // important if backend CORS has credentials:true
                             }
                         );
-      console.log(res.data.data.user.username)
-      return res.data.data.user.username
+                        console.log(res.data.data.user.username)
+                        return res.data.data.user.username
     } catch (err) {
       console.error("Login failed:", err.response?.data || err.message);
     }
