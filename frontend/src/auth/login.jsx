8fe21e147d0d46447020b83a0c25558e3b7f0b7f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:09:18 +0000

    Apr 8, 2026, 10:39 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
                 }}
                 >`Use {way=="email"?"username":"email"} To Login`</h3>
                 <div>
-                    <form>
+                    <form on>
                         <input 
                         className={`${way=="email"?"flex":"hidden"}`}
                         type='text' 
