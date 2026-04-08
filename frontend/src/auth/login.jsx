ba7797c0a0608201c0baf7e52a59ed845814f326
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:22:32 +0000

    Apr 8, 2026, 10:52 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
                     else{setWay('email')}
                 }}
                 >`Use {way=="email"?"username":"email"} To Login`</h3>
-                <div className='h-max'>
+                <div className='h-screen flex items-center justify-center'>
                     <form 
                     onSubmit={HandleSubmit}>
                         <input 
