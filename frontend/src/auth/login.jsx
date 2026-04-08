5a12e84dedddb91d7e4285a355d12aed81323a60
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:08:20 +0000

    Apr 8, 2026, 10:38 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     const [username,setUsername]=useState('');
     const [password,setPassword]=useState('');
     const [way,setWay]=useState('email');
-    async function HandleSubmit(e){
+    async function HandleSubmit(e,credentials){
         e.preventDefault();
         try {
             const res = await axios.post("http://localhost:8000/api/v1/users/login",
