6cd1f4e9c1c56d993aacccc8ce78c8b992a5c884
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:20:22 +0000

    Apr 8, 2026, 10:50 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
                         placeholder='username' value={username} 
                         name='username' onChange={(e)=>{setUsername(e.target.value)}}/>
                         <input 
-                        className='border'
+                        className='border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
                         type='text'
                         value={password}
                         onChange={(e)=>{setPassword(e.target.value)}} 
