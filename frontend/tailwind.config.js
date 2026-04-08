1ec691ead18d7e25a222c501c623aa62aab150d5
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 16:23:21 +0000

    Apr 8, 2026, 9:53 PM

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // adjust paths to your project
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
