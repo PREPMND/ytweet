143ebff10cfb93759bdde7874d849e219f0a1c87
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:20:38 +0000

    Apr 8, 2026, 10:50 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -31,7 +31,9 @@ const Login = () => {
                 }}
                 >`Use {way=="email"?"username":"email"} To Login`</h3>
                 <div>
-                    <form onSubmit={HandleSubmit}>
+                    <form 
+                    
+                    onSubmit={HandleSubmit}>
                         <input 
                         className={`${way=="email"?"flex":"hidden"} border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
                         type='text' 
