d5ca505c2981cbfd6080f6aa1ee45e8ec71157a4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:30:11 +0000

    Apr 8, 2026, 11:00 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
                     </form>
                 
                 </div>
-                <div> </div>
+                <div> Welcome {}</div>
             </div>
         </div>
     </div>
