6289fde771004557d27a9cb6e00e0086d0226dff
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 16:44:32 +0000

    Apr 8, 2026, 10:14 PM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import './index.css'
 import App from './App.jsx'
 import Login from './auth/login.jsx'
 
-    const [email,setEmail]=useState('');
-    const [username,setUsername]=useState('');
 createRoot(document.getElementById('root')).render(
   
   <>
