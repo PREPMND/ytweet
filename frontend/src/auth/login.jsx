89996fed4fa1976d2cf5eb67b7aed3d701b52ab7
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 17:32:14 +0000

    Apr 8, 2026, 11:02 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -14,8 +14,7 @@ const Login = () => {
                         {
                             withCredentials: true,                        
                         });
-            setCurrentUser(res.data.data.user.username)
-;
+            setCurrentUser(res.data.data.user.username);
             return res.data.data.user.username
         } catch (err) {
             console.error("Login failed:", err.response?.data || err.message);}
