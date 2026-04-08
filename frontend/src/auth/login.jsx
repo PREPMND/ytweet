bec28043dcdd215d8c8b5fec2dd3110a5623fcd4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-08 16:14:33 +0000

    Apr 8, 2026, 9:44 PM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-    const [email,setEmail]=use
+    const [email,setEmail]=useState('');
     return (
     <>
     <div>
