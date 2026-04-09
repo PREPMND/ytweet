97d5222868f7b8bf6959ad0b29019b244a6c872d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:45:43 +0000

    Apr 9, 2026, 9:15 AM

--- frontend/src/api/unauth.jsx
+++ frontend/src/api/unauth.jsx
@@ -9,7 +9,7 @@ Api.interceptors.response.use(
     (response)=>response,
     (error)=>{
         if(error.response.status==401){
-            w
+            window.location.href=
         }
     }
 )
