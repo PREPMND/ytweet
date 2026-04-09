7e267f1345e07bc8cfa6510c141220f5996bdb12
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:53:02 +0000

    Apr 9, 2026, 9:23 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
                         className={`${way=="email"?"hidden":"flex"} border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
                         w-[250px]`}
                         type='text' 
-                        placeholder='  Username' value={username} 
+                        placeholder=' Username' value={username} 
                         name='username' onChange={(e)=>{setUsername(e.target.value)}}/>
                         <input 
                         className='border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-[250px]'
