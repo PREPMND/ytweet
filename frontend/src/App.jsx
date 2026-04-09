e3d8cb90454bc84be467c6705dd5a7bb1e765067
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:12:19 +0000

    Apr 9, 2026, 12:42 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import axios from 'axios'
-import { useState ,useEffect} from 'react';
 import AppRoutes from '../routes/routes';
 const App = () => {
   {/*const [username,setUsername]=useState("")
