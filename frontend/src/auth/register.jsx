14c4bc41a5727de93ea9e6d0cc656162f7b403e9
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 06:48:03 +0000

    Apr 9, 2026, 12:18 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -13,7 +13,7 @@ const register = () => {
                             <input type='text' placeholder='Full Name'/>
                             <input type='text' placeholder='Full Name'/>
                             <input type='text' placeholder='Full Name'/>
-                            <input type='file' accept='' placeholder='Full Name'/>
+                            <input type='file' accept='image/*' placeholder='Full Name'/>
                             <input type='text' placeholder='Full Name'/>
 
                         </form>
