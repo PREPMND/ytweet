583f591ecae768e933d233700548872f10ba490f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:15:34 +0000

    Apr 9, 2026, 9:45 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
     <div className='flex justify-center items-center h-screen'>
   
         <Aurora 
-        className='absolute z-0'
+        className='absolute z-0 inset-0'
         colorStops={["#7cff67","#B19EEF","#5227FF"]} blend={0.5} 
         amplitude={10} speed={1}/>
         <div className='border border-amber-300 z-40 rounded-lg w-[400px] h-[400px] flex items-center justify-center'>
