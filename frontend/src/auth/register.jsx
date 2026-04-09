ce4494c9f3d137a636e1f7358a5431d3c862548e
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 13:40:20 +0000

    Apr 9, 2026, 7:10 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
                             type='text' placeholder='Password'/>
                             <input 
                             className='files'
-                            content=''
+                            content='Choose Avatar'
                             type='file' accept='image/*' placeholder='Avatar'/>
                             <input
                             className='files'
