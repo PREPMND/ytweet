1b71f7018ac4e1f47d5bd9c5554f35470035c274
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:51:05 +0000

    Apr 9, 2026, 9:21 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
                 </div>
                 <div
                 className={`
-                    ${currentUser?'hidden'}
+                    ${currentUser?'hidden':'block'}
                     cursor-pointer mt-6 text-blue-500 hover:underline`}
                 >Want to create a new account!</div>
                 <div className={`${currentUser ? 'block' : 'hidden'}`}> Welcome {currentUser}</div>
