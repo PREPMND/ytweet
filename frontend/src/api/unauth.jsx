8501074afcdc24d8bbf2eaa5a9a48408d649c456
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:43:35 +0000

    Apr 9, 2026, 9:13 AM

--- frontend/src/api/unauth.jsx
+++ frontend/src/api/unauth.jsx
@@ -2,5 +2,6 @@ import axios from 'axios'
 import React from 'react'
 
 const Api=axios.create({
-    baseURL:"https://localhost:8000/api/v1/users"
+    baseURL:"https://localhost:8000/api/v1/users",
+    withCredentials
 })
