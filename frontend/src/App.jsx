5d483769335a4075d95953d45f2dc026d342fd64
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:11:24 +0000

    Apr 9, 2026, 12:41 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,7 @@
 import axios from 'axios'
 import { useState ,useEffect} from 'react';
 const App = () => {
-  const [username,setUsername]=useState("")
-  async function loginUser(credentials) {
-    try {
-      const res = await axios.post(
-        "http://localhost:8000/api/v1/users/login",
-        credentials,
-        {
-          withCredentials: true, // important if backend CORS has credentials:true
-        }
-      );
-      console.log(res.data.data.user.username)
-      return res.data.data.user.username
-    } catch (err) {
-      console.error("Login failed:", err.response?.data || err.message);
-    }
-  }
-  // Usage:
-  useEffect(() => {
-    async function fetchUser() {
-      const name = await loginUser({ email: "p@p.com", password: "prep" });
-      if (name) setUsername(name);
-    }
-    fetchUser();
-  }, []);
-
+  {/}
 
 return (
   <div>
