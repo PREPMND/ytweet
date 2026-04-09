ce8b8dee7ba907a6bac70b31ac3675b933199074
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:49:37 +0000

    Apr 9, 2026, 9:19 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
                     </form>
                 
                 </div>
-                <div>Already </div>
+                <div>Want to </div>
                 <div className={`${currentUser ? 'block' : 'hidden'}`}> Welcome {currentUser}</div>
             </div>
         </div>
