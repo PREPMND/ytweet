dec0ea6d5195c6c5eb82b07faaa4800d2e575c32
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:11:46 +0000

    Apr 9, 2026, 9:41 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -27,9 +27,8 @@ const Login = () => {
     <>
 
     <div className='flex justify-center items-center h-screen'>
-        import Aurora from './Aurora';
   
-     <Aurora
+    <Aurora
   colorStops={["#7cff67","#B19EEF","#5227FF"]}
   blend={0.5}
   amplitude={1.0}
