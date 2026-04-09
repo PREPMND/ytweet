2f223ce05d619ee64a24dd694549b01138913193
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:10:52 +0000

    Apr 9, 2026, 12:40 PM

--- frontend/routes/routes.jsx
+++ frontend/routes/routes.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from "react-router-dom"
 import MainLayout from "../layout/mainlayout"
 import AuthLayout from "../layout/authlayout"
 import Login from "../src/auth/login"
+import Register from "../src/auth/register"
 export default function AppRoutes(){
     return(
         <Routes>
