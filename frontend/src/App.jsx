039e4537d38d801f5bb2f5b2961f63f907e9396c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:11:43 +0000

    Apr 9, 2026, 12:41 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useState ,useEffect} from 'react';
 const App = () => {
-  {const [username,setUsername]=useState("")
+  {/*const [username,setUsername]=useState("")
   async function loginUser(credentials) {
     try {
       const res = await axios.post(
