452b991b10cae03fe750c22ce1e7be8632726b1b
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:37:09 +0000

    Apr 9, 2026, 10:07 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -34,8 +34,8 @@ const Login = () => {
         <div style={{ width: '100%', height: '600px', position: 'relative' }}>
         <Particles
         particleColors={["#ffffff"]}
-    particleCount={200}
-    particleSpread={10}
+        particleCount={200}
+        particleSpread={10}
     speed={0.1}
     particleBaseSize={100}
     moveParticlesOnHover
