4269cba5722a059ed5bdd4f9d4abafe238d32c6d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 03:53:55 +0000

    Apr 9, 2026, 9:23 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
                         onChange={(e)=>{setPassword(e.target.value)}} 
                         
                         />
-                        <input type="submit" name='Login' placeholder='Login'/>
+                        <input type="submit" name='Login' bal placeholder='Login'/>
                     </form>
                 
                 </div>
