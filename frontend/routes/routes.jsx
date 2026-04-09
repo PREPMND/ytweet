f1c34a18498b1763ac271275f6a6d98586ed94c0
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:10:03 +0000

    Apr 9, 2026, 12:40 PM

--- frontend/routes/routes.jsx
+++ frontend/routes/routes.jsx
@@ -1,13 +1,14 @@
 import {Routes,Route} from "react-router-dom"
 import MainLayout from "../layout/mainlayout"
 import AuthLayout from "../layout/authlayout"
+import Login from "../src/auth/login"
 export default function AppRoutes(){
     return(
         <Routes>
             <Route element={<MainLayout/>}>
             </Route>
             <Route element={<AuthLayout/>}>
-            <Route element={<Lo}
+            <Route element={<Login/>}
             </Route>
         </Routes>
     )
