f9da82cb1757b39c6cb244402f9a4a61c09eaca0
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 06:50:22 +0000

    Apr 9, 2026, 12:20 PM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './auth/login.jsx'
+import Register from './auth/register.jsx'
 createRoot(document.getElementById('root')).render(
   <>
   <App/>
   <Login/>
-  <Reg
+  <Register/>
   </>
 )
