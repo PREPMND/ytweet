2f623135327a206451a79c66b581f4824c5ead0a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:43:38 +0000

    Apr 9, 2026, 10:13 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
         alphaParticles={false}
         disableRotation={false}
         pixelRatio={1}/>
-        </div>
+        </div>*/
         <div
         className='absolute inset-0 flex z-30 items-center justify-center'>
         <div className='border backdrop-blur-md bg-white/30 border-amber-300 z-30 rounded-lg w-[400px] h-[400px]'>
