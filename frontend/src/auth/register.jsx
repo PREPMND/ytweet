4d7a519d191f177ffdc8f54f2b4c3421ea118622
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 06:46:48 +0000

    Apr 9, 2026, 12:16 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -9,7 +9,8 @@ const register = () => {
                     <h1>Welcome , Register</h1>
                     <div>
                         <form>
-                            <input type='text' placeholder='Full Name'/
+                            <input type='text' placeholder='Full Name'/>
+                            
                         </form>
                     </div>
                 </div>
