e9df22f2425775484bca0a7631cd1c04b22f4951
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 07:02:26 +0000

    Apr 9, 2026, 12:32 PM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Login from './auth/login.jsx'
 import Register from './auth/register.jsx'
 createRoot(document.getElementById('root')).render(
   <>
   <BrowserRouter>
-      <
+    <App/>
   </BrowserRouter>
   </>
 )
