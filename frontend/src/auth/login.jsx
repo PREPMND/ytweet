c65f66860c993ea3c07bd75e34290701bf75e251
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:45:11 +0000

    Apr 9, 2026, 10:15 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -81,7 +81,7 @@ const Login = () => {
                         
                         />
                         <input 
-                        className='bg-blue-300 rounded px-4 py-1 font-[600] text-white '
+                        className='bg-blue-300 placeholder: rounded px-4 py-1 font-[600] text-white '
                         type="submit" name='Login' value={"Login"} placeholder='Login' />
                     </form>
                 
