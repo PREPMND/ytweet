59f14b62d34ba4c736dc38bce354ca44a7093ded
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-09 04:37:50 +0000

    Apr 9, 2026, 10:07 AM

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
     <div className='relative w-screen h-screen'>
 
-        <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <div style={{ width: '100%', height: '85%', position: 'relative' }}>
         <Particles
         particleColors={["#ffffff"]}
         particleCount={200}
