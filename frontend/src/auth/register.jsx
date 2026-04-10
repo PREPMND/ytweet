75ee08014f5c4d6a878aa75a55bbab0e0467c749
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-10 10:34:11 +0000

    Apr 10, 2026, 4:04 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Register = () => {
     function handlingNext(e) {
         e.preventDefault(); // prevent form submit refresh
-        const confirmed = window.confirm("You confirm these details?");
+        const confirmed = window.confirm("You confirm thse details?");
         if (confirmed) {
             setNext(true);
         } else {
