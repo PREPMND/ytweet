cc12ebd1e8afe345e9eac8e0d12fc9bfeeccc9f5
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-10 10:23:09 +0000

    Apr 10, 2026, 3:53 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
                             <input 
                             className='input'
                             type='text' placeholder='Password'/>
-                    
+                            <div
                             <div className={`hidden`}>
                             <div>Choose Avatar</div>
                             <input 
