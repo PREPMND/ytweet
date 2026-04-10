42f57500badd4daef334c95921987ca8d71b4ffe
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-10 10:39:39 +0000

    Apr 10, 2026, 4:09 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react'
 const Register = () => {
 
     function handlingNext(){
-        useEffect(()={
-
+        useEffect(()=>{
+            
         },[next])
     }
     const [next, setNext] = useState(false)
