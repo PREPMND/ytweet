7b100b76eafd7d28d71e22105b1e811542a6cd84
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-10 10:45:25 +0000

    Apr 10, 2026, 4:15 PM

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
   return (
     <div className='w-full h-[100vh] flex items-center justify-center'>
       <form className='flex flex-col gap-8 mb-6 *:w-[280px]'>
-        <div className={`${next}`}>
+        <div className={`${next?"hidden"}`}>
         <input className='input' type='text' placeholder='Full Name' />
         <input className='input' type='text' placeholder='Email' />
         <input className='input' type='text' placeholder='UserName' />
