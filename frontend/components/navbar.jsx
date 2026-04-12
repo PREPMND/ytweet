66e7541645e3bdf4195b1cb2b92350ab5f01f3cc
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 16:40:39 +0530

    modified navbar.jsx in components

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { Home, Loader, LoaderPinwheel } from "lucide-react"
 const navbar = () => {
     return (
         <>
         <div>
             <div>
-                
+                <div>
+                    
+                </div>
             </div>
         </div>
 
