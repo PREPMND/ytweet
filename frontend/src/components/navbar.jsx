656dd2795d14a1b3fde94326b30f47483bd81604
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 12:10:45 +0000

    Apr 12, 2026, 5:40 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
 
-import getCurrentUser from '../api/currentuser';
+import getCurrentUser from '../api/currentuser.jsx';
 const Navbar = () => {
     const currentUser = getCurrentUser();
     console.log(currentUser);
