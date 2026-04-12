d21570ff88a5700cf1e73013a539fc47f0f2a5ad
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 12:51:20 +0000

    Apr 12, 2026, 6:21 PM

--- frontend/src/api/currentuser.jsx
+++ frontend/src/api/currentuser.jsx
@@ -4,7 +4,7 @@ export default function getCurrentUser() {
     async function fetchData() {
         try {
             const response = await axios.get(
-                "http://localhost:8000/api/v1/users/currentuser",
+                "https://localhost:8000/api/v1/users/currentuser",
                 { withCredentials: true }
             );
             console.log(response)
