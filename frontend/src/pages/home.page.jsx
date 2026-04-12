bbafc43f556fa7764d7061b3a31c684bda4fdba2
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 12:06:25 +0000

    Apr 12, 2026, 5:36 PM

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -5,40 +5,40 @@ const Home = () => {
     const [user, setUser] = useState(null);
     const [message, setMessage] = useState("");
 
-        useEffect(() => {
-            async function fetchData() {
-                try {
-                    const response = await axios.get(
-                        "http://localhost:8000/api/v1/users/currentuser",
-                        { withCredentials: true } // <-- critical
-                    );
-                    console.log(response.data)
-                    setUser(response.data.data);
-                    setMessage(response.data.message);
-                } catch (err) {
-                    if( err.status == 401){
-                        console.log(" Please log in.");
-                    }
-                    console.log(err.status);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8000/api/v1/users/currentuser",
+                    { withCredentials: true } // <-- critical
+                );
+                console.log(response.data)
+                setUser(response.data.data);
+                setMessage(response.data.message);
+            } catch (err) {
+                if (err.status == 401) {
+                    console.log(" Please log in.");
                 }
+                console.log(err.status);
             }
-            fetchData();
-        }, []);
+        }
+        fetchData();
+    }, []);
 
-        return (
-            <div>
-                {user ? (
-                    <div>
-                        <h2>{message}</h2>
-                        <p><strong>ID:</strong> {user.id}</p>
-                        <p><strong>Email:</strong> {user.email}</p>
-                        {/* Add more fields as needed */}
-                    </div>
-                ) : (
-                    "Loading..."
-                )}
-            </div>
-        );
-    };
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <h2>{message}</h2>
+                    <p><strong>ID:</strong> {user.id}</p>
+                    <p><strong>Email:</strong> {user.email}</p>
+                    {/* Add more fields as needed */}
+                </div>
+            ) : (
+                "Loading..."
+            )}
+        </div>
+    );
+};
 
-    export default Home;
+export default Home;
