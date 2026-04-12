12cdcfc3f99ceb3a95eaf2e1f865a06ea048119d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 12:58:28 +0000

    Apr 12, 2026, 6:28 PM

--- backendtest/src/app.js
+++ backendtest/src/app.js
@@ -3,11 +3,11 @@ import express from "express";
 import cors from "cors";
 const application = express();
 application.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
-application.use(express.json({limit:"16kb"}))
-application.use(express.urlencoded({extended:true,limit:"16kb"}))
+application.use(express.json({ limit: "16kb" }))
+application.use(express.urlencoded({ extended: true, limit: "16kb" }))
 application.use(express.static("public"))
 application.use(cookieParser())
 
@@ -16,16 +16,16 @@ import router from './routes/user.route.js'
 import routerVideo from "./routes/video.routes.js";
 console.log("YEA")
 
-application.use("/api/v1/users",router)
-application.use("/api/v1/videos",routerVideo )
+application.use("/api/v1/users", router)
+application.use("/api/v1/videos", routerVideo)
 // Global error handler
 application.use((err, req, res, next) => {
-  const status = err.statusCode || 500;
-  res.status(status).json({
-    statusCode: status,
-    message: err.message || "Internal Server Error",
-    success: false
-  });
+    const status = err.statusCode || 500;
+    res.status(status).json({
+        statusCode: status,
+        message: err.message || "Internal Server Error",
+        success: false
+    });
 });
 export { application }
 
