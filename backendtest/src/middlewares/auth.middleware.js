0f28cdc1ee9ed6a113295785158b648a2181816c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 13:24:25 +0000

    Apr 12, 2026, 6:54 PM

--- backendtest/src/middlewares/auth.middleware.js
+++ backendtest/src/middlewares/auth.middleware.js
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken"
 export const verifyJWT = asyncHandler( async (req,res,next)=>{
     console.log("heygwg")
     try {
-        const token =req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        const authHeader = req.header("Authorization");
+        const token =req.cookies?.accessToken || 
         if(!token ){
             throw new apiError(401,"Unauthorized request")
         }
