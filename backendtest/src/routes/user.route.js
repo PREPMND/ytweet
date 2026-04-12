f40bacc7f56003ab3eef60512b0eec51b8e01a33
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 13:46:30 +0000

    Apr 12, 2026, 7:16 PM

--- backendtest/src/routes/user.route.js
+++ backendtest/src/routes/user.route.js
@@ -1,4 +1,6 @@
 import {Router} from "express"
+import { application } from "../app.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { loginUser, logOutUser, refreshAccessToken, registerUser ,changeCurrentPassword, getCurrentUser ,updateAccountDetails,updateUserAvatar,updateUserCoverImage, getUserChannelProfile,getWatchHistory} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
