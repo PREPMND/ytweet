b2b57be4082e73bc87d9cfc63a7e1742104152db
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-12 13:05:19 +0000

    Apr 12, 2026, 6:35 PM

--- backendtest/src/middlewares/auth.middleware.js
+++ backendtest/src/middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 export const verifyJWT = asyncHandler( async (req,res,next)=>{
+    console.log("heygwg")
     try {
         const token =req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
         if(!token ){
