33c11c09a7309c19f5daa684c8965e0313a14353
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:49:08 +0000

    Apr 13, 2026, 12:19 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
                             onMouseEnter={() => setHoverAirplay(true)}
                             onMouseLeave={() => setHoverAirplay(false)}
                         >
-                            <Airplay className={`${hoverAirplay?"text-yellow-400 shadow-lg shadow-yellow-300":""}`}/>
+                            <Airplay className={`${hoverAirplay?"text-yellow-400 shadow-lg shadow-yellow-300 scale-105":""}`}/>
                             <span
                                 className={`text-[10px] mt-1 text-gray-600 font-[600] transition-opacity duration-300 ${hoverAirplay ? "opacity-100" : "opacity-0"
                                     }`}
