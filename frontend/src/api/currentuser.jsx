d0ff7c673334f4612f2cd07434e57b173b54fe5c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 12:25:27 +0000

    Apr 13, 2026, 5:55 PM

--- frontend/src/api/currentuser.jsx
+++ frontend/src/api/currentuser.jsx
@@ -15,8 +15,7 @@ export default function getCurrentUser() {
                 console.log("Error status:", err.response?.status);
                 console.log("Error data:", err.response?.data);
             }
-            return err.response; // Return the error response for further handling
-            // Re-throw the error to be handled by the caller
+            throw err.response; // Re-throw the error to be handled by the caller
         }
     }
     return fetchData();
