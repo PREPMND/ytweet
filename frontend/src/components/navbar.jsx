0a866bc175945ff216da2bf6c54c947d6bbd83d5
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 05:52:09 +0000

    Apr 13, 2026, 11:22 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -13,7 +13,8 @@ const Navbar = () => {
                     <div className="flex items-center select-none ">
                         <img 
                         className="rounded-[100%] object-cover w-10 h-10 ml-5 mt-2 
-                        hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+                        hover:scale- transition-transform duration-300 ease-in-out cursor-pointer
+                        hover:ring-2 hover:ring-blue-500"
                         
                         src={data?.user?.avatar} alt="User Avatar" />
                     </div>
