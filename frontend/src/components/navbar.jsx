b12e2af9fdef33f32c4d46ac96f3ef3dfe87332b
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:04:53 +0000

    Apr 13, 2026, 11:34 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                         About 
                     </div>
                 </div>
-                <div className="w-full border-[1px] ">
+                <div className="w-full border-[1px] shadow-md shadow-orange-50">
 
                 </div>
             </div>
