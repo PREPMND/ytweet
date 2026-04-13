599de9f3dba917a216d393161f9972aa7e0b8f56
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:09:21 +0000

    Apr 13, 2026, 11:39 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import getCurrentUser from '../api/currentuser.jsx';
 import logolight from "../assets/logolight.jpg"
 
-import {Album } from "lucide-react"
+import {Album ,Airplay,Bolt } from "lucide-react"
 const Navbar = () => {
     const { data, error, isLoading } = useQuery({
         queryKey: ["currentUser"],
