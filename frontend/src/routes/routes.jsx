606ed339fcd71f08bab27c4ee4c7775939d6c7a6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 07:08:29 +0000

    Apr 13, 2026, 12:38 PM

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -4,11 +4,12 @@ import AuthLayout from "../layout/authlayout"
 import Login from "../auth/login"
 import Register from "../auth/register"
 import HomePage from "../pages/home.page.jsx"
-export default function AppRoutes(){
+export default function AppRoutes(props){
+    const {User} = props;
     return(
         <Routes>
             <Route element={<MainLayout/>}>
-            <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<HomePage User={User} />}/>
             </Route>
             <Route element={<AuthLayout/>}>
             <Route path="/login" element={<Login/>} />
