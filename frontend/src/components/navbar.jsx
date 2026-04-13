86f10dbea226f7af1138dba967906688627dc6d9
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:21:14 +0000

    Apr 13, 2026, 11:51 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 const Navbar = () => {
     const [hoverBolt,setHoverBolt]=useState(false)
     const [hoverAlbum,setHoverAlbum]=useState(false);
-    
+    const [hover]
     const { data, error, isLoading } = useQuery({
         queryKey: ["currentUser"],
         queryFn: getCurrentUser
