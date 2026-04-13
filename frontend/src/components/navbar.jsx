02cfc96d7bae4fae13cad6df1dacae1039e5dd71
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:52:36 +0000

    Apr 13, 2026, 12:22 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
                         >
                             <Bolt className={`${hoverBolt ? "text-neutral-950" : "text-neutral-700"}`} size={28} />
                             <span
-                                className={`text-[12px] text-gray-600 font-[600] mt-1 transition-opacity duration-300 ${hoverBolt ? "opacity-100" : "opacity-0"
+                                className={`text-[11px] text-gray-600 font-[600] mt-1 transition-opacity duration-300 ${hoverBolt ? "opacity-100" : "opacity-0"
                                     }`}
                             >
                                 PREP
