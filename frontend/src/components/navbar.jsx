39650ed344449780283414bacbb7683631019fc9
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 05:43:07 +0000

    Apr 13, 2026, 11:13 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import { Home, Loader, LoaderPinwheel } from "lucide-react"
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import axios from 'axios';
+import {useQuery} from "@tanstack/react-query";
 
 import getCurrentUser from '../api/currentuser.jsx';
 const Navbar = () => {
-    
+
     return (
         <>
             <div>
