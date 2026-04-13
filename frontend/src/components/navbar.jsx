22ef541ab4154610b1bb4ff666ae614d92db574c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:54:11 +0000

    Apr 13, 2026, 12:24 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
                                 JoinEdge
                             </span>
                         </div>
-                        
+
 
                         <div
                             className="flex flex-col items-center"
