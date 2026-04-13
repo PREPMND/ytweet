e320dfe18236629945c9b9eb2ff6cbc052426556
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 16:22:18 +0000

    Apr 13, 2026, 9:52 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -34,8 +34,14 @@ const VideoList = () => {
                 {videos.map((video) => (
                     <li key={video._id}>
                         <h3>{video.title}</h3>
+                        <img
+                            src={video.thumbnail}
+                            alt={video.title}
+                            width="200"
+                            style={{ display: "block", marginBottom: "10px" }}
+                        />
                         <video width="320" controls>
-                            <source src={video.url} type="video/mp4" />
+                            <source src={video.videoFile} type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
                     </li>
