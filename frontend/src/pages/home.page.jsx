a489827e7910608cb507263d0b30929a653b0521
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 07:14:15 +0000

    Apr 13, 2026, 12:44 PM

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 const Home = (props) => {
     const { User } = props;
-    
+
     return (
         <div>
             {User ? (
                 <div>
-                    <h2>Welcome, {User.user.fullName}!</h2>
-                    <p><strong>ID:</strong> {User.user._id}</p>
-                    <p><strong>Email:</strong> {User.user.email}</p>
-                    <p><strong>Full Name:</strong> {User.user.fullName}</p>
+                    <h2>Welcome, {User.user?.fullName}!</h2>
+                    <p><strong>ID:</strong> {User.user?._id}</p>
+                    <p><strong>Email:</strong> {User.user?.email}</p>
+                    <p><strong>Full Name:</strong> {User.user?.fullName}</p>
                     {/* Add more fields as needed */}
                 </div>
             ) : (
