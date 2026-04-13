80bbe7d4da3e0c7251054810bed1aed61b9566f0
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 06:53:54 +0000

    Apr 13, 2026, 12:23 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -27,6 +27,19 @@ const Navbar = () => {
 
                             src={data?.user?.avatar} alt="User Avatar" />
                         <span className=" text-sm font-medium text-gray-700">{data?.user?.username}</span>
+                        <div
+                            className="flex flex-col items-center"
+                            onMouseEnter={() => setHoverBolt(true)}
+                            onMouseLeave={() => setHoverBolt(false)}
+                        >
+                            <Bolt className={`${hoverBolt ? "text-neutral-950" : "text-neutral-700"}`} size={28} />
+                            <span
+                                className={`text-[11px] text-gray-600 font-[600] mt-1 transition-opacity duration-300 ${hoverBolt ? "opacity-100" : "opacity-0"
+                                    }`}
+                            >
+                                PREP
+                            </span>
+                        </div>
                     </div>
 
 
@@ -49,19 +62,7 @@ const Navbar = () => {
                                 JoinEdge
                             </span>
                         </div>
-                        <div
-                            className="flex flex-col items-center"
-                            onMouseEnter={() => setHoverBolt(true)}
-                            onMouseLeave={() => setHoverBolt(false)}
-                        >
-                            <Bolt className={`${hoverBolt ? "text-neutral-950" : "text-neutral-700"}`} size={28} />
-                            <span
-                                className={`text-[11px] text-gray-600 font-[600] mt-1 transition-opacity duration-300 ${hoverBolt ? "opacity-100" : "opacity-0"
-                                    }`}
-                            >
-                                PREP
-                            </span>
-                        </div>
+                        
 
                         <div
                             className="flex flex-col items-center"
