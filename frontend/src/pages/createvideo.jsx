41630688c27b9b72fbdef3dd4e80f7f536a646df
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-13 16:53:58 +0000

    Apr 13, 2026, 10:23 PM

--- frontend/src/pages/createvideo.jsx
+++ frontend/src/pages/createvideo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import getCurrentUser from '../api/currentuser';
+import getCurrentUser from '../api/currentuser.jsx';
 import axios from "axios";
 import { useQuery } from '@tanstack/react-query';
 const Createvideo = () => {
