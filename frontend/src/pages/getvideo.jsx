5d1c3c27021d16e4f108ea0f17bed149cc8cd4e6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 12:15:49 +0000

    Apr 14, 2026, 5:45 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -134,7 +134,7 @@ const VideoList = () => {
                 ))}
             </div>
 
-            {videos!= null && (
+            {!(videos==null) && (
                 <div
                     style={{
                         marginTop: "30px",
