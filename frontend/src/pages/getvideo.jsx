e278739c9c907ed796712211f6841586f5a20f38
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 12:22:34 +0000

    Apr 14, 2026, 5:52 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -139,7 +139,7 @@ const VideoList = () => {
                     </div>
                 ))}
             </div>
-            <LoaderPinwheel className={`mx-auto animate-spin mt-10 text-white ${data ? "hidden" : ""}`} size={40} />
+            <LoaderPinwheel className={`mx-auto animate-spin flex items-center text-white ${data ? "hidden" : ""}`} size={40} />
             <div
                 className={`${data?"":"hidden"} mt-6  justify-center items-center gap-4`}
                 
