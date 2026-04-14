a6a83396ef69ad18fe9f612486bd9b85090fcdf9
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 02:56:20 +0000

    Apr 14, 2026, 8:26 AM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -59,7 +59,7 @@ const VideoList = () => {
                             clearTimeout(timeoutRef.current);
                             setPlayingId(null);
                         }}
-                        className="overflow-hidden cursor-pointer transition-transform  duration-500 ease-in-out"
+                        className="overflow-hidden cursor-pointer transition-transform relative duration-500 ease-in-out"
                     >
                         {playingId === video._id ? (
                             <video
