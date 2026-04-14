47701f5f6adb41eb57485704f665aa52ee7e6199
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 09:43:31 +0000

    Apr 14, 2026, 3:13 PM

--- frontend/src/layout/mainlayout.jsx
+++ frontend/src/layout/mainlayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 export default function MainLayout(props){
-    const {User} = props;
+    const {menubar, setMenubar} = props;
     return(
         <div>
-            <h2><Navbar User={User} /></h2>
+            <h2><Navbar menubar={menubar} setMenubar={setMenubar} /></h2>
             <div><Outlet/></div>
         </div>
     )
