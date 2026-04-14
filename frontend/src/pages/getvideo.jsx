dc535feb480f1ec43acd1933c99d4f81aa2d483f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 04:16:52 +0000

    Apr 14, 2026, 9:46 AM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -96,10 +96,10 @@ const VideoList = () => {
                         
 
                         <div
-                            className="flex flex-start items-center leading-[1.4] h-[80px] mt-1 ml-2"><img className="min-w-5 max-w-7 max-h-7 min-h-5  rounded-full object-cover" src={video.owner.avatar} alt="Avatar" />
+                            className="flex flex-start items-center leading-[1.4] h-[80px] mt-1 ml-2"><img className="w-9 h-9 shrink-0 rounded-full object-cover" src={video.owner.avatar} alt="Avatar" />
                             <div className="px-[11px]">
                                 <h3
-                                    className="mt-1 text-md text-black leading-[1.4] px-[6px] font-medium capitalize text-ellipsis overflow-hidden whitespace-nowrap"
+                                    className="mt-1 text-md text-black leading-[1.4] px-[6px] font-medium capitalize text-wrap overflow-hidden whitespace-nowrap"
 
                                 >
                                     {video.title}
