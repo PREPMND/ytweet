42c5a7fa45807f472a21476ab4d43a384a79108d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-14 12:17:53 +0000

    Apr 14, 2026, 5:47 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -137,7 +137,7 @@ const VideoList = () => {
             </div>
 
             <div
-                className={`${data==false ? "hidden" : ""} mt-6 flex justify-center items-center gap-4`}
+                className={`${true ? "" : "hidden"} mt-6 flex justify-center items-center gap-4`}
                 style={{
                     marginTop: "30px",
                     display: "flex",
