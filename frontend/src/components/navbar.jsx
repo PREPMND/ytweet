08c9859be30827a57f169e405bc850e8975e36ef
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-15 12:05:17 +0000

    Apr 15, 2026, 5:35 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -141,9 +141,9 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                             <div className= {` ${darkMode?"":"text-stone-900"} text-[12px] text-center mt-2  text-white/70 border-[1px] w-[260px]`}></div>
                             <div>
                                 <div className={`flex items-center transition-colors duration-100 ease-in-out mt-5 ${darkMode ? "text-white hover:text-white" : "text-slate-800 hover:text-black"}`}>
-                                    <VideotapeIcon className=" mr-7" size={28}/>
-                                    <span className="ml-2 text-[22px] font-[500]">Create Videos</span>
-                                    <PlusCircle className={`ml-7 hover:scale-105 hover:text-purple-300 transition-transform duration-300 ease-in-out text-white`}   size={28}/>
+                                    <VideotapeIcon className=" mr-7" size={24}/>
+                                    <span className="ml-2 text-[20px] font-[500]">Create Videos</span>
+                                    <PlusCircle className={`text-right hover:scale-105 hover:text-purple-300 transition-transform duration-300 ease-in-out text-white`}   size={28}/>
                                 </div>
                                 
                             </div>
