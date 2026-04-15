ccdc3a68ecf01fa72357eb8436e6b6fe2ee3f489
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-15 12:10:06 +0000

    Apr 15, 2026, 5:40 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -147,8 +147,8 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                                 </div>
                                 <div className={`flex items-center transition-colors duration-100 ease-in-out mt-5 ${darkMode ? "text-white hover:text-white" : "text-slate-800 hover:text-black"}`}>
                                     <VideotapeIcon className=" mr-7" size={24}/>
-                                    <span className="ml-2 text-[20px] font-[500]">Create Videos</span>
-                                    <PlusCircle className={`ml-10 hover:scale-105 hover:text-white transition-transform duration-300 ease-in-out text-white/80`}   size={28}/>
+                                    <span className="ml-2 text-[20px] font-[500]"></span>
+                                    
                                 </div>
                                 <div className={`flex items-center transition-colors duration-100 ease-in-out mt-5 ${darkMode ? "text-white hover:text-white" : "text-slate-800 hover:text-black"}`}>
                                     <Airplay className=" mr-7" size={24}/>
