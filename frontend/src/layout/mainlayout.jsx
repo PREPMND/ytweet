83c4d0c8ae3411f028c2ab45cb4936e32616e7da
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-15 07:25:37 +0000

    Apr 15, 2026, 12:55 PM

--- frontend/src/layout/mainlayout.jsx
+++ frontend/src/layout/mainlayout.jsx
@@ -4,7 +4,7 @@ export default function MainLayout(props){
     const {menubar, setMenubar, darkMode, setDarkMode, darkModenav, setDarkModenav} = props;
     return(
         <div>
-            <h2><Navbar menubar={menubar} setMenubar={setMenubar} darkModenav={darkModenav} setDarkModenav={setDarkModenav} darkMode={darkMode} setDarkMode={setDarkMode} /></h2>
+            <h2><Navbar menubar={menubar} darkMode={darkMode} setMenubar={setMenubar} darkModenav={darkModenav} setDarkModenav={setDarkModenav} setDarkMode={setDarkMode} /></h2>
             <div><Outlet/></div>
         </div>
     )
