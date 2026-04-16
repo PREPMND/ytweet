b0aa93bc92db86c4afd10b39ef88b26583c13b61
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-16 01:18:18 +0000

    Apr 16, 2026, 6:48 AM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -6,14 +6,31 @@ const ChannelIndv = () => {
         queryKey: ["currentUser"],
         queryFn: getCurrentUser,
     });
-    
+    const fetchVideos = async (pageNum = 1) => {
+        try {
+            setTimeout(() => {
+                setData(false);
+            }, 5000);
+            const res = await fetch(`${import.meta.env.VITE_BACKEND}/api/v1/user/`);
+            const data = await res.json();
+
+            if (data.success) {
+                setVideos(data.data.docs);
+                setData(true);
+            } else {
+                console.error("Backend error:", data.message);
+            }
+        } catch (err) {
+            console.error("Error fetching videos:", err);
+        }
+    };
     return (
         <>
-        <div>
             <div>
-                <img src=''
+                <div>
+                    <img src=''
             </div>
-        </div>
+            </div>
         </>
     )
 }
