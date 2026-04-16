4c2ce1bfa6074352ea6814c3f387f7bd1162754e
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-04-16 01:25:09 +0000

    Apr 16, 2026, 6:55 AM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query';
 const ChannelIndv = () => {
+    const {channelP}
     const [channel,setChannel]=useState(null);
     const { data } = useQuery({
         queryKey: ["currentUser"],
