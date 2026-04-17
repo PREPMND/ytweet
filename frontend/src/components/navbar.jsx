50ef2b0e2b721d9dd8fe4b8f60c87a1df8363dbb
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-04-17 15:50:12 +0000

    Apr 17, 2026, 9:20 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -133,7 +133,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                                 <button className={`border-2 px-3 mt-3 py-1 rounded-[12px] text-[18px] text-neutral-900 font-[400]hover:text-black transition-all duration-300 ease-in-out hover:scale-105  hover:shadow-sm ${darkMode?"text-black bg-white  hover:shadow-fuchsia-400 hover:shadow-md":"hover:shadow-stone-500"}`} >
                                     <span className={`${isLoggedIn==true?"flex":"hidden"}`}>Account Details</span>
                                     <span 
-                                    onClick={()=>{navigate("/login")}}
+                                    onClick={()=>{navigating("/login")}}
                                     className={`${isLoggedIn==false?"flex":"hidden"}`}>Log In</span>
                                 </button>
                             </div>
