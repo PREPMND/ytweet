bdcb4a96dea69bf9477d2277c1ea7231e390ceca
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-04-18 05:42:27 +0000

    Apr 18, 2026, 11:12 AM

---
+++ back/src/controllers/subscription.controller.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../utils/asyncHandler";
+const subscribe = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (channelId === req.user._id.toString()) {
+    throw new apiError(400, "Cannot subscribe to yourself");
+  }
+
+  const sub = await Subscription.create({
+    subscriber: req.user._id,
+    channel: channelId,
+  });
+
+  return res.status(200).json(
+    new apiResponse(200, sub, "Subscribed successfully")
+  );
+});
