e32b8d98cb88a64bd3f4f0f40d6d02ff8763c1c6
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-04-18 06:11:21 +0000

    Apr 18, 2026, 11:41 AM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -26,7 +26,7 @@ const ChannelIndv = () => {
             );
 
             const data = await res.json();
-
+            const subscribed = data.subscribed ?? data.data?.subscribed;
             setChannel((prev) => ({
                 ...prev,
                 isSubscribed: data.subscribed,
