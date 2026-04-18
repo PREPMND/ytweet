aaef5340f6f7dd33e6d48a0a72aaaf414aa370ee
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-04-18 06:01:43 +0000

    Apr 18, 2026, 11:31 AM

--- back/src/controllers/subscription.controller.js
+++ back/src/controllers/subscription.controller.js
@@ -2,28 +2,33 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {apiError} from "../utils/apiError.js"
 import { apiResponse } from "../utils/apiResponse.js";
 import { Subscription } from "../models/subscription.models.js";
-const subscribe = asyncHandler(async (req, res) => {
+const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
 
-  if (channelId === req.user._id.toString()) {
-    throw new apiError(400, "Cannot subscribe to yourself");
-  }
-
-  const sub = await Subscription.create({
+  const existing = await Subscription.findOne({
     subscriber: req.user._id,
     channel: channelId,
   });
 
-  return res.status(200).json(
-    new apiResponse(200, sub, "Subscribed successfully")
-  );
-});
-const unsubscribe = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
+  if (existing) {
+    await existing.deleteOne();
 
-  await Subscription.findOneAndDelete({
+    return res.json({
+      success: true,
+      subscribed: false,
+    });
+  }
+
+  await Subscription.create({
     subscriber: req.user._id,
     channel: channelId,
+  });
+
+  return res.json({
+    success: true,
+    subscribed: true,
+  });
+});    channel: channelId,
   });
 
   return res.status(200).json(
