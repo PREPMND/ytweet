c98126cea30e63aba3a0113694b4ce182436b81e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-04-18 06:05:03 +0000

    Apr 18, 2026, 11:35 AM

--- back/src/controllers/subscription.controller.js
+++ back/src/controllers/subscription.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js"
 import { apiResponse } from "../utils/apiResponse.js";
 import { Subscription } from "../models/subscription.models.js";
-const toggleSubscription = asyncHandler(async (req, res) => {
+export const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
 
     const existing = await Subscription.findOne({
