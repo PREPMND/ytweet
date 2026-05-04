9c3c30ec67e398f5b1a01f47818dcdbc55523811
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-04 07:48:27 +0000

    May 4, 2026, 1:18 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -170,7 +170,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                                     <ChevronDown/>
                                 </button>
                             </div>
-                            <div className= {` ${darkMode?"":"text-stone-900"} text-[12px] text-center mt-3 text-white border-[1px] w-[260px]`}></div>
+                            <div className= {` ${darkMode?"":"text-stone-900"} text-[12px] hidden text-center mt-3 text-white border-[1px] w-[260px]`}></div>
 
                             <button
                                 onClick={() => setDarkMode(!darkMode)}
