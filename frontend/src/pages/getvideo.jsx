6effc26fb107e270e6d1b0ed864294843ef3ec63
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-04 15:19:28 +0000

    May 4, 2026, 8:49 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -24,6 +24,7 @@ const VideoList = (props) => {
 
             if (data.success) {
                 setVideos(data.data.docs);
+                console.log(data.data.docs.title)
                 setData(true);
             } else {
                 console.error("Backend error:", data.message);
