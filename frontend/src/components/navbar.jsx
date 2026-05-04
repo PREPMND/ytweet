a0d2e396d82d6267c79ad6dfe9cfa675d43d20d0
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-04 07:48:45 +0000

    May 4, 2026, 1:18 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -174,7 +174,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
 
                             <button
                                 onClick={() => setDarkMode(!darkMode)}
-                                className="px-3 py-1 text-sm text-center flex justify-center m-auto ml-[44px] mt-4 rounded bg-gray-800 text-white hover:bg-gray-700"
+                                className="px-3 py-1 text-sm text-center flex justify-center mt-7 rounded bg-gray-800 text-white hover:bg-gray-700"
                             >
                                 Toggle Dark Mode
                             </button>
