093a75870659981496ce1fb6035e9100ebabffbc
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-05 10:07:38 +0000

    May 5, 2026, 3:37 PM

--- back/src/routes/video.routes.js
+++ back/src/routes/video.routes.js
@@ -17,6 +17,7 @@ routerVideo.post("/createvideo", verifyJWT,
     ]),
     createVideo);
 console.log("THIS VIDEO ROUTE FILE IS LOADED");
+routerVideo.get("/getvideosbychannel/:id", getVideoByChannel);
 routerVideo.get("/getvideos", getVideos);
 routerVideo.get("/getvideo/:id", getVideoById);
 // use :id as a path param
