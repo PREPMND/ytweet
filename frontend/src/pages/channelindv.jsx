4ecfd9c5d104fd041d74f6c8d1450d235ec5ad67
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-06 05:23:30 +0000

    May 6, 2026, 10:53 AM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -74,7 +74,7 @@ const ChannelIndv = () => {
         }
     };
     const smth =async ()=>{
-        
+        const res=await fetch(`${import.meta.env.VITE_BACKEND}/api/v1/videos/getvideosbychannel`)
     }
     const fetchVideos = async (pageNum = 1) => {
         if (!channel?._id || isLoadingVideos) return;
