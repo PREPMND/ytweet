e5763588144339c571cad930a4951b15267b24f9
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-09 05:11:38 +0000

    May 9, 2026, 10:41 AM

---
+++ frontend/src/api/api.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: `${import.meta.env.VITE_BACKEND}/api/v1`,
+    withCredentials: true,
+});
+
+api.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+
+        const originalRequest = error.config;
+
+        if (
+            error.response?.status === 401 &&
+            !originalRequest._retry
+        ) {
+
+            originalRequest._retry = true;
+
+            try {
+
+                await api.post("/users/refreshtoken");
+
+                return api(originalRequest);
+
+            } catch (refreshError) {
+
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default api;
