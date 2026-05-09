04282793175873be8ce0ce529ed81e4f6dccf50c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-09 07:14:36 +0000

    May 9, 2026, 12:44 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -9,43 +9,7 @@ const ChannelIndv = () => {
         queryFn: getCurrentUser,
     });
     console.log("CURRENT USER DATA:", data);
-    setChannel(data.user);
-    const handleSubscribe = async () => {
-        if (isSubscribing) return;
-        setIsSubscribing(true);
-        try {
-            const res = await fetch(
-                `${import.meta.env.VITE_BACKEND}/api/v1/users/subscriptions/${channel._id}`,
-                {
-                    method: "POST",
-                    credentials: "include",
-                }
-            );
-
-            const text = await res.text();
-            console.log("RAW RESPONSE:", text);
-
-            let data = {};
-            try {
-                data = JSON.parse(text);
-            } catch (e) {
-                console.error("Response is not JSON", e);
-            }
-
-            const subscribed = data.data?.subscribed;
-
-            setChannel((prev) => ({
-                ...prev,
-                isSubscribed: subscribed,
-                subscriberCount:
-                    prev.subscriberCount + (subscribed ? -1 : 1),
-            }));
-        } catch (err) {
-            console.error(err);
-        } finally {
-            setIsSubscribing(false);
-        }
-    };
+    
 
 
     return (
