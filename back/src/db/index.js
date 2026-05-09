86657655d7eb6d72603f1781712536630e39d0b6
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-09 04:59:41 +0000

    May 9, 2026, 10:29 AM

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -3,7 +3,6 @@ import {DBNAME} from "../constant.js";
 export const MongoConnection=async ()=>{
     try{
         const ConnectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DBNAME}`);
-        console.log(`MongoDB is connected on : ${ConnectionInstance.connection.host}`);
     }catch(error){
         console.log("Error in MongoDB connection :",error);
         process.exit(1);
