ba7b4a453aa7e9fc71a8e237cfd8c8cb6a557fd2
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-09 05:12:07 +0000

    May 9, 2026, 10:42 AM

--- frontend/src/api/currentuser.jsx
+++ frontend/src/api/currentuser.jsx
@@ -1,18 +1,8 @@
-import axios from "axios";
-export default function getCurrentUser() {
-    async function fetchData() {
-        try {
-            const response = await axios.get(
-                `${import.meta.env.VITE_BACKEND}/api/v1/users/currentuser`,
-                { withCredentials: true }
-            );
-            return response.data;
-        } catch (err) {
-            if (err.response?.status === 401) {
-                console.log("Please log in.");
-            } 
-            throw err; // Re-throw the error to be handled by the caller
-        }
-    }
-    return fetchData();
+import api from "./api";
+
+export default async function getCurrentUser() {
+
+    const response = await api.get("/users/currentuser");
+
+    return response.data;
 }
