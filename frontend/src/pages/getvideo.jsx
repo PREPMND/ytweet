e01e7e07d835b18b55e5bf1a49ffba3f2202fca4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-12 14:03:22 +0000

    May 12, 2026, 7:33 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -119,7 +119,7 @@ const VideoList = (props) => {
 
                         <div className="flex flex-start items-center leading-[1.4] h-[80px] mt-1 ml-2">
                             <img
-                                onClick={() => { Handle(video._id, video.username) }}
+                                onClick={() => { Handle(${video._id}, video.username) }}
                                 className="w-9 h-9 hover:ring-1 hover:scale-105 hover:ring-blue-300 shrink-0 transition-transform duration-500 ease-in-out rounded-full object-cover"
                                 src={video.owner.avatar}
                                 alt="Avatar"
