c7aae8f332e0d3896d5951d59ef6b1d0870f9511
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-12 12:39:24 +0000

    May 12, 2026, 6:09 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import getCurrentUser from "../api/currentuser";
+import api from "../api/api";
 
 const ChannelIndv = () => {
+    const [videos, setVideos] = useState([]);
     const { data } = useQuery({
         queryKey: ["currentUser"],
         queryFn: getCurrentUser,
     });
     console.log("CURRENT USER DATA:", data);
-    
+    const { videos }=api.get("/videos/any", { owner: data._id });
 
 
     return (
