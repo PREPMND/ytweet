220f8fa539ea6c2ee1f897097bf4f1fa32a08ef3
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-12 13:50:33 +0000

    May 12, 2026, 7:20 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -40,7 +40,7 @@ const ChannelIndv = () => {
                     className="flex justify-between flex-row-reverse"
                     key={video._id || index}>
 
-                        <h3 className="">{video.title}</h3>
+                        <h3 className="mr-9">{video.title}</h3>
                         <img className="h-[200px] object-cover aspect-video w-[40%]" src={video.thumbnail} alt={video.title} />
                     </div>
                 ))}
