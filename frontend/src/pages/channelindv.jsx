83e2efe777af2aa0801beeae0d52887cc146dd31
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-14 15:02:22 +0000

    May 14, 2026, 8:32 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -32,8 +32,8 @@ const ChannelIndv = (props) => {
 
     return (
         <div className="relative">
-            <div>
-                <img  className="-z-30 mt-0 inset-0 absolute w-screen h-[100px] " src={data.user.coverImage} />
+            <div className="">
+                <img  className="-z-30 -mt-5 inset-0 absolute w-screen h-[100px] " src={data.user.coverImage} />
             </div>
             <div className="flex mt-5 md:mt-3 z-40 items-center ">
             {/* the channel description */}
