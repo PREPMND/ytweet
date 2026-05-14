d676be3b71a1f2aaf99872da6e0cbb1dcf6784e1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-14 11:22:47 +0000

    May 14, 2026, 4:52 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -46,8 +46,9 @@ const ChannelIndv = (props) => {
                             <img className="object-cover aspect-[16/9] rounded-md w-[100%] md:w-[50%]" src={video.thumbnail} alt={video.title} />
                             <h3 className={`md:mt-[1px] hidden md:flex font-[600] text-[18px] 
                                 ${video.title.length > 60 ?"whitespace-pre-wrap" : ""}`}>{video.title}</h3>
+                            <EllipsisVertical className="text-[18px]  cursor-pointer absolute right-2 mt-3"  />
                         </div>
-                        <div className="flex items-start md:h-auto h-12 md:px-0 px-2 mb-3 md:mb-0 justify-between md:w-auto w-full gap-2 ">
+                        <div className="flex md:hidden items-start md:h-auto h-12 md:px-0 px-2 mb-3 md:mb-0 justify-between md:w-auto w-full gap-2 ">
                             <h3 className="mt-2 pl-[10px] font-[600] md:hidden flex text-[15px] md:text-[18px]">{video.title}</h3>
                             <EllipsisVertical className="text-[18px]  cursor-pointer absolute right-2 mt-3"  />
                         </div>
