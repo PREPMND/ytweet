0215b5fd00892eca9f6b8992b1281cc124ed64bf
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-14 11:05:01 +0000

    May 14, 2026, 4:35 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -16,7 +16,7 @@ const ChannelIndv = (props) => {
         try {
             const res = await api.post("/videos/any", { owner: ownerId });
             setVideos(res.data.data); // match backend response structure
-            console.log("Videos for channel:", res.data.data.title.length);
+            console.log("Videos for channel:", res.data.data);
         } catch (err) {
             console.error(err);
         }
