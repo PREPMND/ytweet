8697b0bfaebea2b4e732de06336e76cd5d77d4ab
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-14 10:59:57 +0000

    May 14, 2026, 4:29 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -42,7 +42,7 @@ const ChannelIndv = (props) => {
                         key={video._id || index}>
                         
                         <div className="w-full md:w-[80%] flex gap-5 ">
-                            <img className=" object-cover mx-2 aspect-[16/9] rounded-md w-[100%] md:w-[50%]" src={video.thumbnail} alt={video.title} />
+                            <img className=" bject-cover mx-3 aspect-[16/9] rounded-md w-[100%] md:w-[50%]" src={video.thumbnail} alt={video.title} />
                             <h3 className="mt-2 hidden md:flex font-[600] text-[18px]">{video.title}</h3>
                         </div>
                         <div className="flex items-center justify-between
