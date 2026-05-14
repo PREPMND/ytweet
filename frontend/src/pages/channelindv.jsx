38bda381432e8c714ada3319db68e1a3339751fe
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-14 14:46:43 +0000

    May 14, 2026, 8:16 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -36,7 +36,7 @@ const ChannelIndv = (props) => {
             <img className="ml-9 rounded-[100%] w-[100px] md:h-[30%]" src={data.user.avatar} alt={data.user.name} />
             <div className="flex ml-10 justify-between font-[Saira] text-[20px] md:text-[28px] font-[500] w-[70%] items-center h-full ">
                 <div className="">{data.user.username}</div>
-                <div className={`text-[15px] md:text-[22px]`}>{subscribers} subscribers</div>
+                <div className={`text-[15px] mr-4 md:text-[22px]`}>{subscribers} subscribers</div>
             </div>
             </div>
             <div>
