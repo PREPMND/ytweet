226f5ed15251b7c7afe8248e5b4d09458926ff8e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 02:47:18 +0000

    May 15, 2026, 8:17 AM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -53,7 +53,7 @@ const ChannelIndv = (props) => {
                     </div>
 
                     {/* Desktop layout */}
-                    <div className="hidden md:flex md:gap-6">
+                    <div className="hidden md:grid md:grid-cols-1 md:max-w-[40%] md:gap-6">
                         {[...Array(6)].map((_, i) => (
                             <div key={i} className="animate-pulse">
                                 <div className="bg-neutral-300 rounded-md w-full aspect-video"></div>
