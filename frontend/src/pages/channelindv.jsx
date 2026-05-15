8ec5af77fd89d0a6b295b3999fe13d1a565764d7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 11:06:17 +0000

    May 15, 2026, 4:36 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -143,7 +143,7 @@ const ChannelIndv = (props) => {
                                 }}
                                 className="text-[18px]  cursor-pointer hidden md:flex absolute right-1 mt-3" />
                             <MenuDropdown
-                                className="bottom-1 md:bottom-8"
+                                className="bottom-[-10px]"
                                 isOpen={menuOpenId === video._id}
                                 darkMode={darkMode}
                                 items={[
