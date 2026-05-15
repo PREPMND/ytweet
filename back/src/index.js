cfdf02f9e080b50a33b8e61d8a347f273204c1c7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 14:23:34 +0000

    May 15, 2026, 7:53 PM

--- back/src/index.js
+++ back/src/index.js
@@ -5,15 +5,12 @@ dotenv.config({
 })
 import { MongoConnection } from "./db/index.js";
 import { application } from "./app.js";
-console.log(process.env.REFRESH_TOKEN_SECURITY)
 MongoConnection();
 application.use((req, res, next) => {
-    console.log("Incoming:", req.method, req.url);
     next();
 });
 const PORT= process.env.PORT || 3000;
 application.listen(PORT,()=>{
     console.log("Server is running on ", PORT)
-    console.log(listEndpoints(application));
 })
 application.get("/hello", (req, res) => res.send("hello from index"));
