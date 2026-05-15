47cded00ec40ea12ee815980940dab535efdc831
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 12:07:51 +0000

    May 15, 2026, 5:37 PM

--- back/src/controllers/video.controller.js
+++ back/src/controllers/video.controller.js
@@ -13,7 +13,7 @@ export const any = asyncHandler(async (req, res) => {
 
     const user = await User.findById(owner).select("-password -refreshToken -email -createdAt -updatedAt");
 
-    return res.status(200).json({ success: true, data: vidos});
+    return res.status(200).json({ success: true, data: videos});
 });
 export const createVideo = async (req, res) => {
     try {
