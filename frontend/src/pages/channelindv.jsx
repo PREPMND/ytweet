3aa21fa9beb944225280c7bf87a605dc03c42f34
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 10:52:09 +0000

    May 15, 2026, 4:22 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -122,6 +122,32 @@ const ChannelIndv = (props) => {
                             <h3 className="mt-2 pl-[10px] font-[600] md:hidden flex text-[15px] md:text-[18px]">{video.title}</h3>
                             <EllipsisVertical className="text-[18px]  cursor-pointer md:hidden absolute right-2 mt-3" />
                         </div>
+                        <MenuDropdown
+                            isOpen={menuOpenId === video._id}
+                            darkMode={darkMode}
+                            items={[
+                                {
+                                    label: "Watch later",
+                                    onClick: () => handleWatchLater(video),
+                                },
+                                {
+                                    label: "Save to playlist",
+                                    onClick: () => handlePlaylist(video),
+                                },
+                                {
+                                    label: "Share",
+                                    onClick: () => handleShare(video),
+                                },
+                                {
+                                    label: "Not interested",
+                                    onClick: () => handleNotInterested(video),
+                                },
+                                {
+                                    label: "Report",
+                                    onClick: () => handleReport(video),
+                                },
+                            ]}
+                        />
                     </div>
                 ))}
             </div>
