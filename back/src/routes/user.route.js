434d3cd8b089405806a6029ff0f713d915e3aace
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 14:22:37 +0000

    May 15, 2026, 7:52 PM

--- back/src/routes/user.route.js
+++ back/src/routes/user.route.js
@@ -28,13 +28,11 @@ router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refreshtoken").post(refreshAccessToken)
 router.route("/changedpsw").post(verifyJWT,changeCurrentPassword)
 //router.route("/currentuser").get(verifyJWT,getCurrentUser)
-console.log("Token candidate:")
 router.get("/currentuser", verifyJWT, (req, res) => {
     console.log("AFTER VERIFY");
     res.json({ user: req.user || null });
 });
 router.route("/userbyid").post(userById)
-console.log(userById);
 router.route("/updateaccount").patch(verifyJWT,updateAccountDetails)
 router.route("/changeavatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
 
@@ -43,9 +41,5 @@ router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 
 router.post("/subscriptions/:channelId", verifyJWT, toggleSubscription);
-console.log(
-  router.stack
-    .filter(r => r.route)
-    .map(r => r.route.path)
-);
+
 export default router;
