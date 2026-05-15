48d85abd5f41da06fbea4e99a7cfce3db0e5c0fe
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 13:39:49 +0000

    May 15, 2026, 7:09 PM

--- back/src/routes/user.route.js
+++ back/src/routes/user.route.js
@@ -33,7 +33,7 @@ router.get("/currentuser", verifyJWT, (req, res) => {
     console.log("AFTER VERIFY");
     res.json({ user: req.user || null });
 });
-router.route("/userbyid").get(userById)
+router.route("/userbyid").post(userById)
 console.log("Token candidate:");
 router.route("/updateaccount").patch(verifyJWT,updateAccountDetails)
 router.route("/changeavatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
