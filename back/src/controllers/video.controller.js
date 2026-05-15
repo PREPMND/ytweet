8c62456e12fb096e55993dd2f8ac1da79f1da37d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 12:02:21 +0000

    May 15, 2026, 5:32 PM

--- back/src/controllers/video.controller.js
+++ back/src/controllers/video.controller.js
@@ -16,7 +16,7 @@ export const any = asyncHandler(async (req, res) => {
         },
     ]);
 
-    const user = await User.findById(_id);
+    const user = await User.findById(owner).select("-password -refreshToken -email -createdAt -updatedAt");
 
     // attach owner details to each video
     const videosWithOwner = pipelines.map((video) => ({
