f2e578c8a5925148f77fe2e4a99960611c64802e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-05-15 13:37:35 +0000

    May 15, 2026, 7:07 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -19,7 +19,7 @@ const ChannelIndv = (props) => {
     });
     const userById=async(Id)=>{
         try{
-            const res=await api.get("/user/userbyid",{Id:Id});
+            const res=await api.get("/users/userbyid",{Id:Id});
             console.log(res);
         }
         catch(err){
