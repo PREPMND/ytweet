74cb13149e1abb6cf03ad026230dc37acaa9140c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-11 17:04:31 +0000

    Jun 11, 2026, 10:34 PM

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -15,7 +15,7 @@ export default function AppRoutes(props){
             <Route element={<MainLayout menubar={menubar} profileSelected={profileSelected} setProfileSelected={setProfileSelected} darkMode={darkMode} setDarkMode={setDarkMode} setMenubar={setMenubar} darkModenav={darkModenav} setDarkModenav={setDarkModenav} isLoggedIn={isLoggedIn} setisLoggedIn={setisLoggedIn} />}>
             <Route path="/" element={
                 <>
-                <VideoList setDarkMode={setDarkMode} profileSelected={profileSelected} setProfileSelected={setProfileSelected} darkMode={darkMode} />
+                <VideoList setDarkMode={setDarkMode}  profileSelected={profileSelected} setProfileSelected={setProfileSelected} darkMode={darkMode} setvideo />
                 </>}/>
             </Route>
             <Route element={<AuthLayout />}>
