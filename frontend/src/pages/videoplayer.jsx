e787db56b212ac5e42afa6b8bccee76a09ba3394
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-11 17:20:48 +0000

    Jun 11, 2026, 10:50 PM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -1,9 +1,12 @@
-export const VideoIndv =()=>{
+export const VideoIndv =(props)=>{
+    const {videoIdSelected}=props;
+    console.log(videoIdSelected)
     return(
         <>
         <div>
             <div>
                 {/* Actual video player*/}
+                DIJGBOS
                 
             </div>
             <div>
