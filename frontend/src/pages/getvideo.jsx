005d0c27d6d1ddcbb7b3c94c4f5b4a0a1c8cb5ae
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-11 17:08:55 +0000

    Jun 11, 2026, 10:38 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -128,7 +128,8 @@ const VideoList = (props) => {
         setMenuOpenId((prev) => (prev === id ? null : id));
     }
     function GoToVideo(vidDetails){
-        
+        setvideoIdSelected(vidDetails);
+        navigate("/watchvideo")
     }
     return (
         <div
