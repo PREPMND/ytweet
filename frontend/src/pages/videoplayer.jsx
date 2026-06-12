514e4b7bc559d5f38a1627dc0030c2295ead4f03
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-12 05:48:08 +0000

    Jun 12, 2026, 11:18 AM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -1,27 +1,30 @@
-export const VideoIndv =(props)=>{
-    const {videoIdSelected}=props;
+export const VideoIndv = (props) => {
+    const { videoIdSelected } = props;
     console.log(videoIdSelected)
-    return(
+    return (
         <>
-        <div>
             <div>
-                {/* Actual video player*/}
-                DIJGBOS
-                <div className='h-[70%]'>
-                    <video
-                    className="w-full h-[40%] md:h-[60%] bg-black aspect-[16/9] object-contain rounded-lg"
-                    controls>
-                        <source classname="h-[60%]" src={videoIdSelected.videoFile} type="video/mp4"/>
-                    </video>
+                <div>
+                    {/* Actual video player*/}
+                    DIJGBOS
+                    <div className='h-[70%]'>
+
+                        <video
+                            className="w-full h-[40%] md:h-[60%] bg-black aspect-[16/9] object-contain rounded-lg"
+                            controls
+                        >
+                            <source src={videoIdSelected.videoFile} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+                <div>
+                    {/* Comment and likes */}
+                </div>
+                <div>
+                    {/* video suggestions */}
                 </div>
             </div>
-            <div>
-                {/* Comment and likes */}
-            </div>
-            <div>
-                {/* video suggestions */ }
-            </div>
-        </div>
         </>
     )
 }
