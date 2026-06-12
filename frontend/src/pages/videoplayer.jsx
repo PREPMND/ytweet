5450bd07907977f60ea6c647264773a7bfeaadef
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-12 05:49:47 +0000

    Jun 12, 2026, 11:19 AM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -3,11 +3,11 @@ export const VideoIndv = (props) => {
     console.log(videoIdSelected)
     return (
         <>
-            <div>
+            <div className="mt-2 ml-2">
                 <div>
                     {/* Actual video player*/}
                     
-                    <div className='mt-2 ml-2 h-[70%]'>
+                    <div className=' h-[70%]'>
 
                         <video
                             className="w-full md:w-[70%] h-[40%] md:h-[60%] bg-black aspect-[16/9] object-contain rounded-lg"
