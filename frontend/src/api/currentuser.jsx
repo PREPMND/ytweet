f31455c526114bae92254dd9484eb04f1fac8374
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-14 12:30:43 +0000

    Jun 14, 2026, 6:00 PM

--- frontend/src/api/currentuser.jsx
+++ frontend/src/api/currentuser.jsx
@@ -2,5 +2,6 @@ import api from "./api";
 
 export default async function getCurrentUser() {
     const response = await api.get("/users/currentuser");
+    console.log("Current User Data:", response.data);
     return response.data;
 }
