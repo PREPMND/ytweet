ed30923ac42cb2a52622fbfd14a93cf36ed0c044
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-14 12:09:12 +0000

    Jun 14, 2026, 5:39 PM

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -192,7 +192,6 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
         sameSite:"lax",
         maxAge:7 * 24 * 60 * 60 * 1000
     };
-    console.log("before generate");
     const { accessToken, refreshToken }=await generateAccessAndRefreshTokens(user._id);
 
     return res
