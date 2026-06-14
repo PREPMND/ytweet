75706f59041f7f03c6bc0e82d762f3d74e59b3c1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-14 12:07:59 +0000

    Jun 14, 2026, 5:37 PM

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -176,7 +176,7 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
     );
 
     const user = await User.findById(decodedToken?._id);
-
+    console.log("user")
 
     if (!user) {
         throw new apiError(401,"Invalid RefreshToken");
