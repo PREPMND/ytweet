a8620540cd225fce50d3480fe52d1e8f8a6f510f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-14 12:04:26 +0000

    Jun 14, 2026, 5:34 PM

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -167,7 +167,7 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
         req.cookies?.refreshToken || req.body?.refreshToken;
 
     console.log(req.cookies);
-    console.log("incoming", incomingRefreshToken);
+    console.log("incoming\n", incomingRefreshToken);
 
     if (!incomingRefreshToken) {
         throw new apiError(401,"Unauthorized Request");
@@ -178,11 +178,11 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
         process.env.REFRESH_TOKEN_SECURITY
     );
 
-    console.log("decoded", decodedToken);
+    console.log("decoded\n", decodedToken);
 
     const user = await User.findById(decodedToken?._id);
 
-    console.log("db token", user?.refreshToken);
+    console.log("db token\n", user?.refreshToken);
 
     if (!user) {
         throw new apiError(401,"Invalid RefreshToken");
