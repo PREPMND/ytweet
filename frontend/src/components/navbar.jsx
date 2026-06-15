7f91cd399b79169e63b4a2915d5be7eaba0c8fa3
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-15 04:31:13 +0000

    Jun 15, 2026, 10:01 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -202,7 +202,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                             <div className={` ${darkMode ? "" : "text-stone-900"} text-[12px] hidden text-center mt-3 text-white border w-64`}>
                             </div>
 
-                            <div className="w-full mb-3 ">
+                            <div className="w-full mb-3 ml-5">
                                 <button
                                     onClick={HandleLogout}
                                     className={`w-32 h-8 text-sm text-center flex justify-center items-center mt-7 rounded bg-red-600 text-white hover:bg-red-500 ${isLoggedIn ? "flex" : "hidden"}`}
