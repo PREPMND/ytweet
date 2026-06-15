f9b79fe7198788cadacb7a95ce86db1c0d030b8a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-15 04:42:27 +0000

    Jun 15, 2026, 10:12 AM

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,4 +15,4 @@ createRoot(document.getElementById('root')).render(
   </QueryClientProvider>
   </>
 )
-{/* VITE_BACKEND= /VITE_BACKEND=https://render-jr5.onrender.com*/}
+{/* VITE_BACKEND= /VITE_BACKEND=https://render-jr65.onrender.com*/}
