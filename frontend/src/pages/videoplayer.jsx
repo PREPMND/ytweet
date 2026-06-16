c2a843b52f8f4488209bc4826b78a60025829dd7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-16 16:05:50 +0000

    Jun 16, 2026, 9:35 PM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -21,8 +21,9 @@ export const VideoIndv = () => {
 
         // fetch owner's other videos
         setLoadingVideos(true);
-        const res2 = await api.post("/videos/any", { owner: res.data.data._id });
+        const res2 = await api.post("/videos/any", { owner: res.data.data });
         setVideos(res2.data.data);
+        console.log("Owner's other videos:", res2.data.data);
         setLoadingVideos(false);
       } catch (err) {
         console.error(err);
