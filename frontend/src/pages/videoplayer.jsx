9d12fe63ae0e3b9340df7eac9846bbc75b09ba31
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-16 16:21:22 +0000

    Jun 16, 2026, 9:51 PM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -16,7 +16,7 @@ export const VideoIndv = () => {
   useEffect(() => {
     const fetchVideo = async () => {
       try {
-        const res = await api.get(`/videos/watchvideo/${id}`);
+        const res = await api.get(`/videos/getvideo/${id}`);
         setVideo(res.data.data);
         console.log("Fetched video data:", res.data);
 
