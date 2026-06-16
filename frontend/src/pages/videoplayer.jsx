f44944d2c467ada6abea12b4c9a2ad9f49369612
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-16 16:27:21 +0000

    Jun 16, 2026, 9:57 PM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -101,7 +101,7 @@ export const VideoIndv = () => {
                                 <img
                                     src={v.thumbnail}
                                     alt={v.title}
-                                    className="w-full rounded-lg aspect-video h-full object-cover"
+                                    className="w-full md:w-[50%] rounded-lg aspect-video h-full object-cover"
                                 />
                                 <span className="absolute bottom-2 right-2 bg-black bg-opacity-70 text-white text-xs px-2 py-1 rounded">
                                     {v.durationFormatted}
