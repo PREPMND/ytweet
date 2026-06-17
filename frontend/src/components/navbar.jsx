45bc61eb8e7c2828f920d80292e296b5b5bc4abc
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-17 03:16:33 +0000

    Jun 17, 2026, 8:46 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
     useEffect(() => {
         // Whenever the route changes to "/", reset menu state
         if (location.pathname === "/") {
-            setNavigate(false);
+            setMenubar(false);
         }
     }, [location])
     useEffect(() => {
