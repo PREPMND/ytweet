29c45abd94336d7330bd18089c54dcbae48aab90
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-17 02:59:59 +0000

    Jun 17, 2026, 8:29 AM

--- frontend/src/pages/changepassword.jsx
+++ frontend/src/pages/changepassword.jsx
@@ -37,7 +37,7 @@ const ChangePassword = ({ darkMode }) => {
       setNewPassword("");
       setConfirmPassword("");
 
-      navigate("/currentuserdetails");
+      navigate("/-1");
     } catch (err) {
       console.log(err);
       alert(
