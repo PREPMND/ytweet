d95335b359f89e14e915602b53a79587f1ce8fe3
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-17 01:33:48 +0000

    Jun 17, 2026, 7:03 AM

--- frontend/src/pages/managevideo.jsx
+++ frontend/src/pages/managevideo.jsx
@@ -1,8 +1,10 @@
+import {useNavigate} from "react-router-dom"
 export default function ManageVideo(){
+    const navigate = useNavigate();
     return(
         <div>
             <h1>Manage Video Page</h1>
-            <
+            <h3 onClick={() => navigate("/updatevideo/:id")}>Update Video</h3>
         </div>
     )
 }
