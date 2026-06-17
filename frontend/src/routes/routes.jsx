f6861dc93638498a60d4fa27c982df24135ff869
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-17 02:38:43 +0000

    Jun 17, 2026, 8:08 AM

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -11,6 +11,7 @@ import { VideoIndv } from "../pages/videoplayer.jsx"
 import ManageVideo from "../pages/managevideo.jsx"
 import UpdateVideo from "../pages/updatevideo.jsx"
 import AccountPage from "../pages/accountpage.jsx"
+import UpdateProfile from "../pages/updateuserdetails.jsx"
 export default function AppRoutes(props){
     const {menubar,setMenubar,darkModenav,setDarkModenav,darkMode,setDarkMode,profileSelected,setProfileSelected, setisLoggedIn,isLoggedIn,videoIdSelected,setvideoIdSelected } = props;
     return(
