ed23c54ad9f5a89f1da4ec278410fc1dbcc9098f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-17 01:25:21 +0000

    Jun 17, 2026, 6:55 AM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -179,6 +179,11 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
 
                                 </div>
                                 <div className={`flex items-center transition-colors duration-100 ease-in-out mt-5 ${darkMode ? "text-white hover:text-white/95" : "text-slate-900 hover:text-black"}`}>
+                                    <Album className="mr-5 md:mr-7" size={24} />
+                                    <span className="ml-2 hover:scale-105 transition-transform duration-200 ease-out md:text-[20px] text-[15px] font-[500]">Manage Videos</span>
+
+                                </div>
+                                <div className={`flex items-center transition-colors duration-100 ease-in-out mt-5 ${darkMode ? "text-white hover:text-white/95" : "text-slate-900 hover:text-black"}`}>
                                     <Bolt className="mr-5 md:mr-7" size={24} />
                                     <span className="ml-2 hover:scale-105 transition-transform duration-200 ease-out md:text-[20px] text-[15px] font-[500]">Enter PREP!</span>
 
