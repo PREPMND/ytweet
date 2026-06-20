8cbb8097730914042b48eb5d59f8724a45918f82
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-20 05:38:24 +0000

    Jun 20, 2026, 11:08 AM

--- frontend/src/pages/accountpage.jsx
+++ frontend/src/pages/accountpage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
 import getCurrentUser from "../api/currentuser";
-import GridScan from "../assets/gridscan.jsx"
+import{ GridScan} from "../assets/gridscan.jsx"
 const AccountPage = ({ darkMode }) => {
     const navigate = useNavigate();
 
