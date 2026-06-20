ad7f008cd148d7674f6d03329d3f91e3ad111d41
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-20 06:30:20 +0000

    Jun 20, 2026, 12:00 PM

--- frontend/src/assets/loading2.jsx
+++ frontend/src/assets/loading2.jsx
@@ -57,7 +57,7 @@ export default function LoaderTwo({ dark, text , username }) {
         />
 
         <h1 className="mt-6 text-white text-3xl font-bold tracking-[0.3em]">
-          {username}
+          
         </h1>
 
         <p className="mt-3 text-white/60 tracking-widest">
