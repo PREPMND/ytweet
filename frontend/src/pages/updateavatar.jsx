d7158c51e219106c41aa18352a4b56981d269b85
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-20 07:25:10 +0000

    Jun 20, 2026, 12:55 PM

--- frontend/src/pages/updateavatar.jsx
+++ frontend/src/pages/updateavatar.jsx
@@ -74,7 +74,7 @@ const ChangeAvatar = ({ darkMode }) => {
             <button
               type="submit"
               disabled={loading}
-              className="px-5 py-3 hover:bg-blue-700 bg-blue-600 text-white rounded-lg"
+              className="px-5 py-3 hover:bg-blue-700 transition-colors duration-300 ease-in-out bg-blue-600 text-white rounded-lg"
             >
               {loading ? "Uploading..." : "Update Avatar"}
             </button>
