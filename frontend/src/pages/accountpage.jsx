82b2738ddd563e751686a2f31eeeb046d9c2354f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-20 05:57:07 +0000

    Jun 20, 2026, 11:27 AM

--- frontend/src/pages/accountpage.jsx
+++ frontend/src/pages/accountpage.jsx
@@ -64,7 +64,7 @@ const AccountPage = ({ darkMode }) => {
     if (loading) {
         return (
             <div className="flex relative justify-center w-full items-center h-screen">
-                <GridScan className={"w-full object-cover"} darkMode={darkMode} />
+                <GridScan className={"w-full object-cover text-black"} darkMode={darkMode} />
                 <h1 className="text-3xl absolute text-white z-40 font-[Saira] font-bold animate-pulse mt-4">
                     Loading 
                 </h1>
