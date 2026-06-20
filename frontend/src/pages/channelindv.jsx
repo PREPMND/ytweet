9bb9bd9f70de7d652175fe8c87411bc5fe70fa73
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-20 07:03:30 +0000

    Jun 20, 2026, 12:33 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -229,7 +229,10 @@ ${yangSelected
                         </div>
 
                         <div className="flex md:hidden items-start md:h-auto h-12 md:px-0 px-2 mb-3 md:mb-0 justify-between md:w-auto w-full gap-2 ">
-                            <h3 className="mt-2 pl-[10px] font-[600] md:hidden flex text-[15px] md:text-[18px]">{video.title}</h3>
+                            <h3
+                                className={`mt-2 pl-[10px] font-[600] md:hidden flex text-[15px]
+${darkMode ? "text-white" : "text-black"}`}
+                            >{video.title}</h3>
                             <EllipsisVertical
                                 onClick={(e) => {
                                     e.stopPropagation();
