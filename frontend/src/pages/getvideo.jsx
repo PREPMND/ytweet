1a51b2a260f61b822a865f272c03efc6b135c035
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-21 09:51:41 +0000

    Jun 21, 2026, 3:21 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -201,7 +201,7 @@ const VideoList = (props) => {
                                     >
                                         <h3
 
-                                            className={`md:text-[15px] text-[16px] leading-5 font-medium line-clamp-2 break-words ${darkMode
+                                            className={`md:text-[14px] text-[16px] leading-5 font-medium line-clamp-2 break-words ${darkMode
                                                 ? "text-white"
                                                 : "text-black"
                                                 }`}
