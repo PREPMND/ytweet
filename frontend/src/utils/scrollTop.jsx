497f3dc4897acb792787badb20c0015d755ea831
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-21 08:50:18 +0000

    Jun 21, 2026, 2:20 PM

--- frontend/src/utils/scrollTop.jsx
+++ frontend/src/utils/scrollTop.jsx
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
- 
-export function ScrollToTop=()=>{
 
-}
+export const ScrollToTop = () => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
+};
