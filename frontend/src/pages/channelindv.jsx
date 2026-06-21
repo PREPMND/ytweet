630e0d1f00359507c581007f0fe38a56a7140ec9
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-21 09:32:50 +0000

    Jun 21, 2026, 3:02 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -130,7 +130,7 @@ const ChannelIndv = (props) => {
 
                     <img className="ml-4 md:ml-9 mb-4 rounded-full object-cover aspect-square
                 w-[100px] h-[100px] md:h-[20%] md:w-[20%]" src={profileSelected.owner.avatar} alt={profileSelected.owner.username} />
-                    <div className="flex pl-6 md:pl-10 justify-between font-[Saira] text-[20px] md:text-[28px] font-[500]  w-[70%] items-center h-[120px] ">
+                    <div className="flex pl-4 md:pl-10 justify-between font-[Saira] text-[20px] md:text-[28px] font-[500]  w-[70%] items-center h-[120px] ">
                         <div className={`${darkMode ? "text-white" : "text-black"}`}>
                             {profileSelected.owner.username}
                         </div>
