df83daf9d1497f9cfeff402f4f9d0c26d605c1e0
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-21 09:35:44 +0000

    Jun 21, 2026, 3:05 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -189,7 +189,7 @@ ${yangSelected
                                 alt={video.title}
                             />
                             <h3
-                                className={`md:mt-[1px] ml-5 hidden md:flex font-[600] text-[18px]
+                                className={`md:mt-[1px] mt-3 ml-5 hidden md:flex font-[600] text-[18px]
                                 ${darkMode ? "text-white" : "text-black"}
                                 ${video.title.length > 70 ? "whitespace-pre-wrap" : ""}`}
                             >{video.title}</h3>
