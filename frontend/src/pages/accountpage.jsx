ea4f6f0356c16f26dd594e29f21d36f51f6a763b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-22 07:17:00 +0000

    Jun 22, 2026, 12:47 PM

--- frontend/src/pages/accountpage.jsx
+++ frontend/src/pages/accountpage.jsx
@@ -81,7 +81,7 @@ const AccountPage = ({ darkMode }) => {
         >
 
             <div
-                className={`max-w-5xl *:select-none mx-auto rounded-2xl p-6 ${darkMode ? "bg-neutral-900" : "bg-white"
+                className={`max-w-full *:select-none mx-auto rounded-2xl p-6 ${darkMode ? "bg-neutral-900" : "bg-white"
                     }`}
             >
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
