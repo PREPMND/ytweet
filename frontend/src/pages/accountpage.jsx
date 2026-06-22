2eb705b9ef220b9e30ddd02232d25f2809a17dc6
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-22 07:40:45 +0000

    Jun 22, 2026, 1:10 PM

--- frontend/src/pages/accountpage.jsx
+++ frontend/src/pages/accountpage.jsx
@@ -182,6 +182,7 @@ const AccountPage = ({ darkMode }) => {
                                     }`}
                             >
                                 <img
+                                 
                                     src={video.thumbnail}
                                     alt={video.title}
                                     className="w-full md:w-[50%]  h-48 md:h-56 object-cover"
