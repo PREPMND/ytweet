60c73d92440eaa6f2a4aefcaa4d98d126560a82d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-23 14:50:22 +0000

    Jun 23, 2026, 8:20 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -30,19 +30,7 @@ const VideoList = (props) => {
         staleTime: 1000 * 60 * 10,
         refetchOnWindowFocus: true,
     });
-    const preloadImages = (urls) => {
-        return Promise.all(
-            urls.map(
-                (url) =>
-                    new Promise((resolve) => {
-                        const img = new Image();
-                        img.src = url;
-                        img.onload = resolve;
-                        img.onerror = resolve;
-                    })
-            )
-        );
-    };
+    
 
 
 
