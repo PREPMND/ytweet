01ea863253986f8f962f097f383fdb6dbfd6d5b0
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-23 14:17:28 +0000

    Jun 23, 2026, 7:47 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -149,11 +149,9 @@ const VideoList = (props) => {
                             className={`overflow-hidden rounded-2xl cursor-pointer transition-all duration-300 ease-out ${darkMode ? "bg-black" : "bg-white"
                                 }`}
                         >
-                            
                             <div className="relative aspect-video select-none overflow-hidden rounded-xl bg-black">
                                 {playingId === video._id ? (
-                                    !thumbnailLoading && (
-                                        <video
+                                    <video
 
                                         src={video.videoFile}
                                         autoPlay
@@ -163,7 +161,8 @@ const VideoList = (props) => {
                                         preload="metadata"
                                         className="w-full h-full object-cover"
                                     />
-                                ) : (
+                                ) : {!thumbnailLoading && (
+
                                     <img
 
                                         onClick={() => { GoToVideo(video) }}
@@ -174,7 +173,7 @@ const VideoList = (props) => {
                                         alt={video.title}
                                         className="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-[1.02]"
                                     />
-                                )}
+                                )}}
 
                                 <span
                                     className="absolute bottom-2 right-2 bg-black/90 text-white text-[13px] md:text-[12px]  font-[500] px-2 py-[2px] rounded"
