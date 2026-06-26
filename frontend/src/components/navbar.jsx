4c456e104a5f094724d54bac18171b6efc1b781e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 13:41:57 +0000

    Jun 26, 2026, 7:11 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -245,7 +245,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                     <ScanSearch className="" size={28}/>
                 </form>
                 <div className="text-[18px] font-[Saira]">Messages</div>
-                <MessageCircleDashedIcon className="hover:scale-105 hover:text-amber-200 mr-1 ml-4"/>
+                <MessageCircleDashedIcon className="hover:scale-110 hover:text-amber-200 mr-1 ml-4"/>
             </div>
         </>
     )
