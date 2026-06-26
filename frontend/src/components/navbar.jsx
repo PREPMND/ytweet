f76c77f1400930ba1b1f7a8d021ba0acc1aa9491
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 13:17:05 +0000

    Jun 26, 2026, 6:47 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -128,7 +128,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
 
             <div className="md:hidden h-[7%] z-50 bg-neutral-900 fixed bottom-0 w-full flex items-center justify-evenly">
 
-                    <SquareDashedText className={ `hover:scale-[1.02] transition-transform duration-300 ease-in-out text-yellow-50 `}/>
+                    <SquareDashedText className={ `hover:scale-[1.05] transition-transform duration-300 ease-in-out text-yellow-50 `}/>
 
 
                     <ScanSearch className={`text-white `}/>
