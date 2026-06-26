a83ece422f10cf51961676bcb119340f1b06fabe
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 13:24:24 +0000

    Jun 26, 2026, 6:54 PM

--- frontend/src/pages/getvideo.jsx
+++ frontend/src/pages/getvideo.jsx
@@ -108,7 +108,7 @@ const VideoList = (props) => {
 
     return (
         <div
-            className={`relative overflow-hidden md:pb-[70px] min-h-screen cursor-pointer transition-all duration-300 ease-out will-change-transform pt-9 transform-gpu ${darkMode ? "bg-black" : "bg-white"
+            className={`relative overflow-hidden md:pb-[70px] pb-[50px] min-h-screen cursor-pointer transition-all duration-300 ease-out will-change-transform pt-9 transform-gpu ${darkMode ? "bg-black" : "bg-white"
                 }`}
         >
             <div
