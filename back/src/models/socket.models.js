62343b15e40b98722ddd4b63723984b062acdb5a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 12:40:19 +0000

    Jun 26, 2026, 6:10 PM

--- back/src/models/socket.models.js
+++ back/src/models/socket.models.js
@@ -1,9 +1,15 @@
 import {Mongoose, Schema,model} from "mongoose";
+import { User } from "./user.models";
 const socketSchema=new Schema(
     {
         sender:{
-            type:"string",
-            
-        }
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        },
+        receiver:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        },
+        conver
     },{timestamps:"true"}
 )
