cc9193121767c3aca908045b68e95f64734a42ce
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 13:38:49 +0000

    Jun 26, 2026, 7:08 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -240,12 +240,12 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                 </div>
             )}
             <div className={`hidden md:flex items-center w-full pb-1  px-10 ${darkMode ? "bg-black  text-white" : ""}`}>
-                <form className="w-full flex">
+                <form className="w-full flex items-center">
                     <input className={`w-[70%] pl-8 h-9 ${darkMode?"bg-neutral-800 text-white":"text-black bg-slate-300"}`} type="text"/>
-                    <ScanSearch className=""/>
+                    <ScanSearch className="" size={30}/>
                 </form>
-                
-                <MessageCircleDashedIcon/>
+                <div>Messages></div>
+                <MessageCircleDashedIcon className="mr-3"/>
             </div>
         </>
     )
