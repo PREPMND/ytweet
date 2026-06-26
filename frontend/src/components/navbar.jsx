835e24ff8450bd559e2e41acb11df0cc88b52eb7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-26 13:34:20 +0000

    Jun 26, 2026, 7:04 PM

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -239,7 +239,7 @@ const Navbar = ({ menubar, setMenubar, darkModenav, setDarkModenav, darkMode, se
                     </div>
                 </div>
             )}
-            <div className={` hidden md:flex w-full  px-10 ${darkMode ? "bg-black  text-white" : ""}`}>
+            <div className={` hidden md:flex w-full pb-1  px-10 ${darkMode ? "bg-black  text-white" : ""}`}>
                 <form className="w-full">
                     <input className={`w-full h-9 ${darkMode?"bg-neutral-900 text-white":"text-black bg-slate-300"}`} type="text"/>
                 </form>
