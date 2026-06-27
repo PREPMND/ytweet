3e2d59d84777d065602ca1c495addbac3db3d58f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-27 04:00:30 +0000

    Jun 27, 2026, 9:30 AM

--- back/src/routes/socket.route.js
+++ back/src/routes/socket.route.js
@@ -1,5 +1,22 @@
-import {express} from "express";
-const routerSocket=express.Router();
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  sendMessage,
+  getMessages,
+  getConversations,
+} from "../controllers/message.controller.js";
 
-routerSocket.post()
+const routerSocket = Router();
+
+routerSocket.use(verifyJWT);
+
+// Send a message
+routerSocket.post("/send", sendMessage);
+
+// Get all messages of a conversation
+routerSocket.get("/:conversationId", getMessages);
+
+// Get all conversations of current user
+routerSocket.get("/", getConversations);
+
 export default routerSocket;
