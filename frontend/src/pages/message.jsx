7a86ad7d45c88f510334aa36e773e36aff0874eb
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-27 03:49:34 +0000

    Jun 27, 2026, 9:19 AM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -1,24 +1,26 @@
 import { useEffect } from "react";
 import { socket } from "../socket";
-export default function Messages(){
-    const roomId="fwrge"
+export default function Messages() {
+    const roomId = "fwrge"
     useEffect(() => {
         socket.connect();
         socket.on("connect", () => {
-          
+
         });
         socket.emit("join-room", roomId);
-        socket.emit("send-message",roomId,"hey hello")
-        
-      }, []);
-    return(
+        socket.emit("send-message", roomId, "hey hello")
+        socket.on("receive-message", (message) => {
+            console.log(message);
+        });
+    }, []);
+    return (
         <>
-        <div className=" flex justify center mt-10 mb-10">
-            <input type="text" className="m-auto bg-black text-white h-11 "/>
-        </div>
-        <div className="text-center">
-            IHWGEUH
-        </div>
+            <div className=" flex justify center mt-10 mb-10">
+                <input type="text" className="m-auto bg-black text-white h-11 " />
+            </div>
+            <div className="text-center">
+                IHWGEUH
+            </div>
         </>
     )
 }
