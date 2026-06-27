3e9f77ecf0a642ecbd60146415184fca48e2b12e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-27 03:27:04 +0000

    Jun 27, 2026, 8:57 AM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -3,9 +3,9 @@ export default function Messages(){
     return(
         <>
         <div className=" flex justify center mt-10 mb-10">
-            <input type="text"/>
+            <input type="text" className="m-auto bg-black text-white h-11 "/>
         </div>
-        <div>
+        <div className="text-center">
             IHWGEUH
         </div>
         </>
