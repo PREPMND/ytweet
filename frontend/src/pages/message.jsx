ada4be5646c357462437e60170a62e4c2d13530c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-27 03:23:15 +0000

    Jun 27, 2026, 8:53 AM

---
+++ frontend/src/pages/message.jsx
@@ -0,0 +1,13 @@
+
+export default function Messages(){
+    return(
+        <>
+        <div className=" flex justify center mt-10 mb-10">
+            <input type="text"/>
+        </div>
+        <div>
+            IHWGEUH
+        </div>
+        </>
+    )
+}
