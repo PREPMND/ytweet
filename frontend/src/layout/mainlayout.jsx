b8dbe1b96d80f074797f8d7f853d90bbf26e8c84
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-06-27 08:27:58 +0000

    Jun 27, 2026, 1:57 PM

--- frontend/src/layout/mainlayout.jsx
+++ frontend/src/layout/mainlayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 export default function MainLayout(props){
-    const {menubar, setMenubar, darkMode, setDarkMode, darkModenav, setDarkModenav,isLoggedIn,setisLoggedIn} = props;
+    const {menubar, setMenubar, darkMode, currentId, setcurrentId,  setDarkMode, darkModenav, setDarkModenav,isLoggedIn,setisLoggedIn} = props;
     return(
         <div>
-            <h2><Navbar menubar={menubar} darkMode={darkMode} setMenubar={setMenubar} darkModenav={darkModenav} setDarkModenav={setDarkModenav} setDarkMode={setDarkMode} isLoggedIn={isLoggedIn} setisLoggedIn={setisLoggedIn} /></h2>
+            <h2><Navbar menubar={menubar} currentId={currentId} setcurrentId={setcurrentId} darkMode={darkMode} setMenubar={setMenubar} darkModenav={darkModenav} setDarkModenav={setDarkModenav} setDarkMode={setDarkMode} isLoggedIn={isLoggedIn} setisLoggedIn={setisLoggedIn} /></h2>
             <div><Outlet/></div>
         </div>
     )
