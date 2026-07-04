294f829da4ed32b9e6f8e85ca909dd31ec58f6e2
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-04 11:47:24 +0000

    Jul 4, 2026, 5:17 PM

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:9000",{
+export const socket = io("https://socketconn-ebp8.onrender.com",{
     autoConnect:false,
     transports: ["websocket"],
     withCredentials: true,
