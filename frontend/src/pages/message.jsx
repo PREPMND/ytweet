621828fde4471619b191f8c243d1d1958585a744
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-04 11:40:26 +0000

    Jul 4, 2026, 5:10 PM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -92,6 +92,9 @@ export default function Messages({ currentId }) {
                 {messages.map((msg, i) => (
 
                     <div key={i}>
+                        if (ms) {
+                            
+                        }
                         {msg.text}
                     </div>
 
