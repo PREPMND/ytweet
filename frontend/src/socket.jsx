b71acdb8bd30f1d074b74673d750cc9307c5aad1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-04 11:47:53 +0000

    Jul 4, 2026, 5:17 PM

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://socketconn-ebp8.onrender.com",{
+export const socket = io(import.meta.env.VITE_SOCKET_BACKEND,{
     autoConnect:false,
     transports: ["websocket"],
     withCredentials: true,
