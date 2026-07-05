38f4cca61da257b3f4870ee777f1e681cb88bd6a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-05 12:36:05 +0000

    Jul 5, 2026, 6:06 PM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -87,7 +87,7 @@ export default function Messages({ currentId }) {
 
             <h2 className={`mt-2 ml-4 text-xl font-[Saira]`}>Messages</h2>
 
-            <div className={`w-full h-[calc(100%-50px)] overflow-y-auto flex flex-col gap-2 p-2 pl-4 pr-4 no-scrollbar`}>
+            <div className={`w-full h-[calc(100%-50px)] overflow-y-auto flex flex-col gap-2 p-2 pl-4 pr-4 pb-18 no-scrollbar`}>
 
                 {messages.map((msg, i) => (
 
