9875e701c6d9c1e115a1187ac81099285acdfd02
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-05 07:56:24 +0000

    Jul 5, 2026, 1:26 PM

--- back/src/app.js
+++ back/src/app.js
@@ -35,6 +35,17 @@ application.use((err, req, res, next) => {
         errors: err.errors || []
     });
 });
+application._router.stack.forEach((layer) => {
+    if (layer.route) {
+        console.log(layer.route.path);
+    } else if (layer.name === "router") {
+        layer.handle.stack.forEach((handler) => {
+            if (handler.route) {
+                console.log(handler.route.path);
+            }
+        });
+    }
+});
 export { application }
 
 
