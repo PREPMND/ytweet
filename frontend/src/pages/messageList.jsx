5693ea6170b8fb09fc908c1bcc881aecc7880501
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-05 07:21:15 +0000

    Jul 5, 2026, 12:51 PM

--- frontend/src/pages/messageList.jsx
+++ frontend/src/pages/messageList.jsx
@@ -34,7 +34,7 @@ export default function MessageList() {
             {conversations.map((chat) => (
 
                 <div
-                    onCLick={() => navigate(`/message/${chat.receiverId}`)}
+                    onClick={() => navigate(`/message/${chat.receiverId}`)}
                     key={chat._id}
                     className="border p-3 mb-2 cursor-pointer"
                 >
