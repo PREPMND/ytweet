e1f0574d8c3e0a40c550ff1e46deadd9d6fef3e5
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-07 02:54:37 +0000

    Jul 7, 2026, 8:24 AM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -97,9 +97,16 @@ export default function Messages({ currentId, darkMode }) {
 
         <div className={`w-full h-[100dvh] flex flex-col no-scrollbar`}   >
             <div className={`w-full h-[65px] md:pl-8 flex items-center border-b ${darkMode ? "bg-black text-white border-neutral-700" : "bg-white text-black border-neutral-300"}`}>
-                <div className="flex items-center">
                 <img src={receiverInfo?.avatar || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"} alt={receiverInfo?.fullName || "User"} className="w-10 h-10 rounded-full object-cover ml-4 " />
                 <h2 className={` ml-4 text-xl font-[Saira]`}>{receiverInfo?.fullName || "User"}</h2>
+                <div>
+                    {receiverInfo?.isOnline ? (
+                        <span className="ml-2 text-sm text-green-500">Online</span>
+                    ) : ( 
+                        <span className="ml-2 text-sm text-gray-500">Offline</span>
+                    )}
+                </div>
+                
             </div>
 
             <div className={`w-full h-[calc(100%-50px)] overflow-y-auto flex flex-col gap-2 p-2 pl-4 pr-4 pb-20 no-scrollbar`}>
