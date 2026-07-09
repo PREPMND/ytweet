7d5c0997657ba8a951c558dac2f7b916ff4f0aa4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-09 09:59:58 +0000

    Jul 9, 2026, 3:29 PM

---
+++ back/src/middlewares/ratelimiter.middleware.js
@@ -0,0 +1,22 @@
+import client from "../redis/redis.js";
+
+export const rateLimiter = async (req, res, next) => {
+    try {
+        const key = req.user
+            ?`rate:${req.user._id}`:`rate:${req.ip}`;
+        const requests = await client.incr(key);
+        if (requests === 1) {
+            await client.expire(key, 60); // 60 seconds
+        }
+        if (requests > 20) {
+            return res.status(429).json({
+                success: false,
+                message: "Too many requests. Please try again later.",
+            });
+        }
+        next();
+    } catch (err) {
+        console.log("Rate Limiter Error:", err);
+        next();
+    }
+};
