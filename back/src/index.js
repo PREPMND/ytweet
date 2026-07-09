bb4d2c6615fa1cd096ed8bce75b10be9644ed80a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-09 09:42:59 +0000

    Jul 9, 2026, 3:12 PM

--- back/src/index.js
+++ back/src/index.js
@@ -6,6 +6,7 @@ dotenv.config({
 import { MongoConnection } from "./db/index.js";
 import { application } from "./app.js";
 MongoConnection();
+await connectRedis();
 application.use((req, res, next) => {
     next();
 });
