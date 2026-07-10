e5df625b640bd72bcaaaabb7f798e942e4829f91
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-10 07:26:38 +0000

    Jul 10, 2026, 12:56 PM

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -150,14 +150,17 @@ export default function Messages({ currentId, darkMode }) {
             <div className={`w-full h-[calc(100%-50px)] overflow-y-auto flex flex-col gap-2 p-2 pl-4 pr-4 pb-20 no-scrollbar`}>
 
                 {messages.map((msg, i) => (
+                    const senderId =
+                        typeof msg.sender === "object"
+                            ? msg.sender._id
+                            : msg.sender;
+                <div
+                    ref={bottomRef}
+                    className={`w-fit max-w-[75%]  break-words px-2 py-1 rounded-md ${msg.sender == currentId ? "bg-blue-400 border-[1.5px] border-cyan-400  text-white self-end" : "bg-gray-300 border-slate-400 text-black self-start"} text-[17px] border-[1.5px]`}
+                    key={i}>
 
-                    <div
-                        ref={bottomRef}
-                        className={`w-fit max-w-[75%]  break-words px-2 py-1 rounded-md ${msg.sender == currentId ? "bg-blue-400 border-[1.5px] border-cyan-400  text-white self-end" : "bg-gray-300 border-slate-400 text-black self-start"} text-[17px] border-[1.5px]`}
-                        key={i}>
-
-                        {msg.text}
-                    </div>
+                    {msg.text}
+                </div>
 
                 ))}
 
