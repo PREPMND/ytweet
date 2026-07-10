ad945638434d87ade47837240a1c4485b706147f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-10 08:31:54 +0000

    Jul 10, 2026, 2:01 PM

--- frontend/src/pages/videoplayer.jsx
+++ frontend/src/pages/videoplayer.jsx
@@ -60,12 +60,12 @@ export const VideoIndv = ({ darkMode }) => {
             </div>
             {desOpen && (
                 <div
-                    className={`fixed inset-0 ${darkMode ? "bg-neutral-900 text-white" : "bg-white"} bg-opacity-50 z-40
+                    className={`fixed inset-0  bg-opacity-50 z-40
                         `}
                     onClick={() => setDesOpen(false)}
                 >
                     <div
-                        className={`absolute bottom-0 w-[100%] left-0 right-0 md:top-[30%] md:h-auto h-[60%] md:mx-auto md:w-[80%] pt-4 pl-5 ${darkMode ? " bg-neutral-950 text-white" : "bg-white"}   bg-white rounded-t-lg p-4 z-50
+                        className={`absolute bottom-0  w-[100%] left-0 right-0 md:top-[30%] md:h-auto h-[60%] md:mx-auto md:w-[80%] pt-4 pl-5 ${darkMode ? " bg-neutral-950 text-white" : "bg-white"}   bg-white rounded-t-lg p-4 z-50
                                     ${desOpen ? 'translate-y-0' : 'translate-y-full'} transition-transform  duration-1000 ease-in-out`}
                         onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
                     >
