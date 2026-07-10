b0c7c455950cbcc5cd0b15e35add64f6b25a1153
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-10 07:57:26 +0000

    Jul 10, 2026, 1:27 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -36,7 +36,7 @@ const ChannelIndv = (props) => {
     const fetchuserById = async (userId) => {
         if (!userId) return;
         try {
-            const res = await api.post("/users/userbyid", { userId });
+            const res = await api.post("/users/userbyid", { userId:profileSelected.owner._id });
             setcoverImage(res.data.data.coverImage)
         }
         catch (err) {
@@ -46,7 +46,7 @@ const ChannelIndv = (props) => {
     const channelVideo = async (ownerId) => {
         setLoadingVideos(true);
         try {
-            const res = await api.post("/videos/any", { owner: ownerId });
+            const res = await api.post("/videos/any", { owner: profileSelected.owner._id });
             setVideos(res.data.data);
             // match backend response structure
             setLoadingVideos(false);
