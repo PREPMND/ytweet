021b6d5f851ea325d2d135239908d4d6583c9c7d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-11 07:04:48 +0000

    Jul 11, 2026, 12:34 PM

--- back/src/middlewares/cache.middleware.js
+++ back/src/middlewares/cache.middleware.js
@@ -10,9 +10,10 @@ export const cache = (expiry = 300) => {
             const cachedData = await redisClient.get(key);
 
             if (cachedData) {
+                console.log(key);
                 return res.status(200).json(JSON.parse(cachedData));
             }
-
+            console.log(key);
             const originalJson = res.json.bind(res);
 
             res.json = async (data) => {
