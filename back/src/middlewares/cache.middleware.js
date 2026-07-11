db9515c794e1010d3cd5f9aa7b5143fee1761280
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-11 07:25:09 +0000

    Jul 11, 2026, 12:55 PM

--- back/src/middlewares/cache.middleware.js
+++ back/src/middlewares/cache.middleware.js
@@ -3,17 +3,17 @@ import redisClient from "../redis/redis.js";
 export const cache = (expiry = 300) => {
     return async (req, res, next) => {
 
-        const key = `${req.method}:${req.originalUrl}:${JSON.stringify(req.body ?? {})}`;
+        const key = `${req.method}:${req.originalUrl}:user:${req.user?._id ?? "public"}:${JSON.stringify(req.body ?? {})}`;
 
         try {
 
             const cachedData = await redisClient.get(key);
 
             if (cachedData) {
-                console.log("hit",key);
+                console.log("hit", key);
                 return res.status(200).json(JSON.parse(cachedData));
             }
-            console.log("miss",key);
+            console.log("miss", key);
             const originalJson = res.json.bind(res);
 
             res.json = async (data) => {
