8982483d2fc2de1e783f4e57c22b89c31dc5d768
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-11 07:06:01 +0000

    Jul 11, 2026, 12:36 PM

--- back/src/routes/user.route.js
+++ back/src/routes/user.route.js
@@ -32,7 +32,7 @@ router.route("/changedpsw").post(verifyJWT,changeCurrentPassword)
 router.get("/currentuser", verifyJWT, (req, res) => {
     res.json({ user: req.user || null });
 });
-router.route("/userbyid").post(userById)
+router.route("/userbyid").post(cache(300), userById)
 router.route("/updateaccount").patch(verifyJWT,updateAccountDetails)
 router.route("/changeavatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
 router.route("/getchannel").post(verifyJWT,getUserChannelProfile)
