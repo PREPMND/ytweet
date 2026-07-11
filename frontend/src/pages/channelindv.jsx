1fd8933e8af273add109eceb301fc6b2de7317fd
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-11 07:31:19 +0000

    Jul 11, 2026, 1:01 PM

--- frontend/src/pages/channelindv.jsx
+++ frontend/src/pages/channelindv.jsx
@@ -189,7 +189,7 @@ const ChannelIndv = (props) => {
                     </div>
 
                     <div className="flex mt-4 justify-evenly">
-                        <button onClick={() => setLocalSubscriptionStatus(!localSubscriptionStatus)} className={`bg-[#cc0000] ${localSubscriptionStatus ? "bg-neutral-700" : ""} text-white text-[15px] w-[100px] md:w-[120px] text-center md:text-[18px] px-2 mr-2 md:mr-4 transition-all duration-500 ease-in-out py-1 rounded-[12px] `}>
+                        <button onClick={() => handleSubscription(channel?._id)} className={`bg-[#cc0000] ${localSubscriptionStatus ? "bg-neutral-700" : ""} text-white text-[15px] w-[100px] md:w-[120px] text-center md:text-[18px] px-2 mr-2 md:mr-4 transition-all duration-500 ease-in-out py-1 rounded-[12px] `}>
                             {localSubscriptionStatus ? "Unsubscribe " : "Subscribe"}
                         </button>
                         <button className={` text-white text-[15px] w-[100px] md:w-[120px] text-center md:text-[18px] px-2 mr-2 md:mr-4 transition-all duration-500 ease-in-out py-1 rounded-[12px]`}>
